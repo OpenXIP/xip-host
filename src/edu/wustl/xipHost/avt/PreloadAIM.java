84db4eac1a04caf5e41c5c2f89fd6463602ebb24
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-03-14 21:31:59 +0000

    PreloadAIM modified to try to associate AIM with a particular DICOM object.
    SVN-Revision: 69

--- src/edu/wustl/xipHost/avt/PreloadAIM.java
+++ src/edu/wustl/xipHost/avt/PreloadAIM.java
@@ -5,8 +5,18 @@ package edu.wustl.xipHost.avt;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.siemens.scr.avt.ad.annotation.AnnotationFactory;
 import com.siemens.scr.avt.ad.annotation.ImageAnnotation;
+import com.siemens.scr.avt.ad.dicom.GeneralImage;
 import com.siemens.scr.avt.ad.io.AnnotationBatchLoader;
+import com.siemens.scr.avt.ad.io.AnnotationIO;
+import com.siemens.scr.avt.ad.query.Queries;
+import com.siemens.scr.avt.ad.util.HibernateUtil;
+
 import edu.wustl.xipHost.localFileSystem.HostFileChooser;
 
 /**
@@ -17,6 +27,7 @@ public class PreloadAIM extends AnnotationBatchLoader{
 
 	public PreloadAIM(){
 		HostFileChooser fileChooser = new HostFileChooser(true, new File("./dicom-dataset-demo"));
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		fileChooser.setVisible(true);
 		File[] files = fileChooser.getSelectedItems();
 		if(files == null){
@@ -25,8 +36,16 @@ public class PreloadAIM extends AnnotationBatchLoader{
 		long time1 = System.currentTimeMillis();
 		for(int i = 0; i < files.length; i++){
 			try {												 
-				ImageAnnotation t = this.readFromFile(files[i]);
-				this.loadSingleObject(t);	
+				ImageAnnotation aim = this.readFromFile(files[i]);
+				AnnotationFactory factory = AnnotationIO.getFactory();
+				List<String> refs = factory.parseReferencedSOPInstanceUIDs(factory.parseDoc(files[i]));
+				for(String imageUID : refs){
+					GeneralImage image = Queries.findImage(imageUID, session);
+					aim.getReferencedImages().add(image);
+					image.getAnnotations().add(aim);
+				}
+				//this.loadSingleObject(aim);
+				HibernateUtil.saveOrUpdate(aim, session);				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -34,7 +53,8 @@ public class PreloadAIM extends AnnotationBatchLoader{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}				
+		}	
+		session.close();
 		long time2 = System.currentTimeMillis();
 		System.out.println("*********** AIM preload SUCCESSFUL *****************");
 		System.out.println("Total load time: " + (time2 - time1)/1000+ " s");
