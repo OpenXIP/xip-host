d4037eaaf41342d9d51c8e1d7dc391b43fd1d55c
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-03-14 17:29:27 +0000

    DICOM retrieve and AIM retrieve are performed in one thread, together. AVT Retrieve modified to retrieve object from the new AVT AD.
    SVN-Revision: 64

--- src/edu/wustl/xipHost/avt/AVTRetrieve.java
+++ src/edu/wustl/xipHost/avt/AVTRetrieve.java
@@ -6,9 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
 import org.jdom.Document;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
@@ -16,6 +19,7 @@ import org.jdom.output.XMLOutputter;
 
 import com.siemens.scr.avt.ad.annotation.ImageAnnotation;
 import com.siemens.scr.avt.ad.api.ADFacade;
+import com.siemens.scr.avt.ad.io.DicomIO;
 
 public class AVTRetrieve implements Runnable{
 	ADFacade adService = AVTFactory.getADServiceInstance();
@@ -59,44 +63,57 @@ public class AVTRetrieve implements Runnable{
 	
 	SAXBuilder builder = new SAXBuilder();
 	Document document;
+	XMLOutputter outToXMLFile = new XMLOutputter();
 	List<File> retrieve() throws IOException {		
-		String[] retrievedAIM = null;
-		List<File> files = new ArrayList<File>();
-		if(aimUID != null){
-			retrievedAIM = new String[1];
-			ImageAnnotation annotation = adService.getAnnotation(aimUID);
-			String strXML = annotation.getAIM();
-			retrievedAIM[0] = strXML;			
-		}else{
-			List<ImageAnnotation> retrievedAIMs = adService.retrieveAnnotationsInSeries(seriesInstanceUID);
-			retrievedAIMs.toArray(retrievedAIM);
-		}	
-		File inputDir = File.createTempFile("AIM-XIPHOST", null, importDir);
+		File inputDir = File.createTempFile("AVT-XIPHOST", null, importDir);
 		importDir = inputDir;
 		inputDir.delete();
 		if(!importDir.exists()){
 			importDir.mkdir();
+		}		
+		String dirPath = "";
+		if(importDir.getCanonicalPath().endsWith(File.separator)){
+			dirPath = importDir.getCanonicalPath();
+		}else{
+			dirPath = importDir.getCanonicalPath() + File.separator;
 		}
-		for(int i = 0; i < retrievedAIM.length; i++){									
-			String fileName = null;
-			try {
-				byte[] source = retrievedAIM[i].getBytes();
-				InputStream is = new ByteArrayInputStream(source);
-				document = builder.build(is);				
-				fileName = document.getRootElement().getAttribute("uid").getValue() + ".xml";
-			} catch (JDOMException e) {
+		List<File> files = new ArrayList<File>();
+		HashMap<Integer, Object> dicomCriteria = new HashMap<Integer, Object>();
+		dicomCriteria.put(Tag.StudyInstanceUID, studyInstanceUID);
+		dicomCriteria.put(Tag.SeriesInstanceUID, seriesInstanceUID);		
+		//Retrieve DICOM
+		List<String> dicomUIDs = adService.findDicomObjs(dicomCriteria, null);										
+		for(int i = 0; i < dicomUIDs.size(); i++){			
+			try {				
+				String fileName = dirPath + dicomUIDs.get(i) + ".dcm";
+				File file = new File(fileName);
+				DicomIO.dumpDicom2File(dicomUIDs.get(i), fileName);
+				files.add(file);
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}			
-			//File outFile = new File(this.importDir + File.separator + fileName);
-			File outFile = new File(this.importDir + File.separator + UUID.randomUUID().toString() + ".xml");
-			FileOutputStream outStream = new FileOutputStream(outFile);
-			XMLOutputter outToXMLFile = new XMLOutputter();
+		}			
+		//Retrieve AIM		
+		List<String> annotationUIDs = adService.findAnnotations(dicomCriteria, null);		
+		for(int i = 0; i < annotationUIDs.size(); i++){						
+			ImageAnnotation loadedAnnot = adService.getAnnotation(annotationUIDs.get(i));			
+			String strXML = loadedAnnot.getAIM();
+			byte[] source = strXML.getBytes();
+			InputStream is = new ByteArrayInputStream(source);
+			try {
+				document = builder.build(is);
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+			File outFile = new File(dirPath + UUID.randomUUID().toString() + ".xml");
+			FileOutputStream outStream = new FileOutputStream(outFile);			
 			outToXMLFile.output(document, outStream);
 	    	outStream.flush();
 	    	outStream.close();
 	    	files.add(outFile);
-		}										
+		}									
 		return files;		
 	}		
 	
