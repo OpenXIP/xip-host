051079dc2babc90585bd4ad167d070db3a536247
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-03-30 19:18:57 +0000

    RemoveAIMDataTest removed. caGrid provides support for AIM_rv09 whereas other packages were updated to AIM_rv12.
    SVN-Revision: 86

--- src-tests/edu/wustl/xipHost/caGrid/TestcaGridPkgeAll.java
+++ src-tests/edu/wustl/xipHost/caGrid/TestcaGridPkgeAll.java
@@ -21,8 +21,7 @@ public class TestcaGridPkgeAll {
 		suite.addTestSuite(MapDicomTagToNCIATagNameTest.class);
 		suite.addTestSuite(ModifyGridLocationTest.class);
 		suite.addTestSuite(QueryGridLocationTest.class);
-		suite.addTestSuite(RemoveGridLocationTest.class);
-		suite.addTestSuite(RetrieveAimDataTest.class);
+		suite.addTestSuite(RemoveGridLocationTest.class);		
 		suite.addTestSuite(RetrieveDicomDataTest.class);
 		suite.addTestSuite(RunGridStartupSequenceTest.class);
 		suite.addTestSuite(StoreGridLocationsTest.class);		
