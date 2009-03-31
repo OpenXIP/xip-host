df915e5d8da481ad23db409b7b23479f818c4988
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-03-31 15:13:12 +0000

    Modified StoreAIMToAD - unit test not finished yet.
    SVN-Revision: 95

--- src-tests/edu/wustl/xipHost/avt/StoreAIMToADTest.java
+++ src-tests/edu/wustl/xipHost/avt/StoreAIMToADTest.java
@@ -4,22 +4,19 @@
 package edu.wustl.xipHost.avt;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import edu.wustl.xipApplication.aim.AimParseEvent;
-import edu.wustl.xipApplication.aim.AimParseListener;
-import edu.wustl.xipApplication.aim.AimParser;
 import junit.framework.TestCase;
 
 /**
  * @author Jaroslaw Krych
  *
  */
-public class StoreAIMToADTest extends TestCase implements AimParseListener {
+public class StoreAIMToADTest extends TestCase {
 	AVTStore avtStore;
-	List<AimParser> aimResults;
 	int numThreads = 3;
 	ExecutorService exeService = Executors.newFixedThreadPool(numThreads);
 	
@@ -33,43 +30,16 @@ public class StoreAIMToADTest extends TestCase implements AimParseListener {
 
 	//AVTStore 1A - basic flow. Perfect condistions. AIM objects to store are valid XML strings.
 	//Expected result: boolean true
-	public void testStoreAimToAD_1A() {
-		String[] aims = new String[4];
-		List<File> files = new ArrayList<File>();
-		File aim1 = new File("./src-tests/edu/wustl/xipHost/avt/0022BaselineA.xml");
-		File aim2 = new File("./src-tests/edu/wustl/xipHost/avt/0022BaselineB.xml");
-		File aim3 = new File("./src-tests/edu/wustl/xipHost/avt/0022FollowupA.xml");
-		File aim4 = new File("./src-tests/edu/wustl/xipHost/avt/0022FollowupB.xml");
-		files.add(aim1);
-		files.add(aim2);
-		files.add(aim3);
-		files.add(aim4);
-		aimResults = new ArrayList<AimParser>();
-		parse(files);
-		while(aimResults.size() != 4){
-			
-		}
-		for(int i = 0; i < aimResults.size(); i++){
-			aims[i] = aimResults.get(i).getXMLString();
-			System.out.println(aims[i]);
-			System.out.println("-----------------------------------------------------------------");
-		}		
+	public void testStoreAimToAD_1A(){
+		File[] aims = new File[3];		
+		File aim1 = new File("./test-content/AIM_rv12_Test/0022BaselineA.xml");		
+		File aim2 = new File("./test-content/AIM_rv12_Test/0022FollowupA.xml");		
+		File aim3 = new File("./test-content/AIM_rv12_Test/0022FollowupB.xml");
+		aims[0] = aim1;
+		aims[1] = aim2;
+		aims[2] = aim3;				
 		avtStore = new AVTStore(aims);
 		avtStore.run();
 		assertTrue(avtStore.getStoreResult());
 	}
-	
-	void parse(List<File> items){
-		for(int i = 0; i < items.size(); i++){									
-			AimParser parser = new AimParser(items.get(i));
-			parser.addAimParseListener(this);						
-			exeService.execute(parser);			
-		}	
-		//exeService.shutdown();
-	}
-
-	public void parsedAimAvailable(AimParseEvent e) {
-		AimParser aimParser = (AimParser)e.getSource();
-		aimResults.add(aimParser);		
-	}	
 }
