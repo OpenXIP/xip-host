b0782b9de4dd83a2aa08451b6f44dce21d90f45a
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-03-31 15:01:55 +0000

    AIM string path replaced with File.
    SVN-Revision: 90

--- src/edu/wustl/xipHost/avt/AVTStore.java
+++ src/edu/wustl/xipHost/avt/AVTStore.java
@@ -3,21 +3,17 @@
  */
 package edu.wustl.xipHost.avt;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import org.jdom.JDOMException;
 import org.nema.dicom.wg23.ObjectLocator;
-
 import com.siemens.scr.avt.ad.annotation.ImageAnnotation;
 import com.siemens.scr.avt.ad.api.ADFacade;
+import com.siemens.scr.avt.ad.io.AnnotationIO;
 
 /**
  * @author Jaroslaw Krych
@@ -25,37 +21,22 @@ import com.siemens.scr.avt.ad.api.ADFacade;
  */
 public class AVTStore implements Runnable {
 	ADFacade adService = AVTFactory.getADServiceInstance();
-	String[] aimToStore;		
+	File[] aimToStore;		
 	
-	public AVTStore(String[] aimToStore){
+	public AVTStore(File[] aimToStore){
 		this.aimToStore = aimToStore;
 	}
-	
+		
 	public AVTStore(List<ObjectLocator> aimObjectLocs){
-		aimToStore = new String[aimObjectLocs.size()];
+		aimToStore = new File[aimObjectLocs.size()];
 		for(int i = 0; i < aimObjectLocs.size(); i++){
 			String aimStrURI = aimObjectLocs.get(i).getUri();
 			URI uri;
 			try {
 				uri = new URI(aimStrURI);
-				File file = new File(uri);			    
-				FileInputStream fs = new FileInputStream(file);			    
-			    DataInputStream in = new DataInputStream(fs);
-			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			    String strLine;
-			    String aimXML = new String();			    
-			    while ((strLine = br.readLine()) != null){			     
-			      aimXML = aimXML + strLine;			 
-			    }
-			    in.close();
-			    aimToStore[i] = aimXML;
+				File file = new File(uri);			    				
+			    aimToStore[i] = file;
 			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}					
@@ -67,8 +48,17 @@ public class AVTStore implements Runnable {
 		List<ImageAnnotation> imageAnnotations = new ArrayList<ImageAnnotation>();
 		int i = aimToStore.length;
 		for(int j = 0; j < i; j++){
-			ImageAnnotation imageAnnotation = new ImageAnnotation();			
-			imageAnnotation.setAIM(aimToStore[j]);
+			try {
+				ImageAnnotation imageAnnotation = AnnotationIO.loadAnnotationFromFile(aimToStore[j]);
+				imageAnnotations.add(imageAnnotation);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		bln = adService.saveAnnotations(imageAnnotations, null, null);
 		notifyStoreResult(bln);
