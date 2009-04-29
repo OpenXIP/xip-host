bde4184345ca7f758d40e3b6bc210bed46bedf98
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-04-29 16:18:53 +0000

    Simulating SVN conflict. Learning and testing.
    SVN-Revision: 140

--- src/edu/wustl/xipHost/hostControl/HostConfigurator.java
+++ src/edu/wustl/xipHost/hostControl/HostConfigurator.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2008 Washington University in Saint Louis. All Rights Reserved.
- * Simulating SVN Conflict
+ * Simulating SVN Conflict, 2
  */
 package edu.wustl.xipHost.hostControl;
 
