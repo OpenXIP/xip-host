4b845abe26dc942d932a9b0c5635f8887400ff46
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-09-20 13:32:25 +0000

    query method was added to GridQuery (moved from GridManagerImpl).
    SVN-Revision: 196

--- src/edu/wustl/xipHost/caGrid/GridQuery.java
+++ src/edu/wustl/xipHost/caGrid/GridQuery.java
@@ -9,9 +9,17 @@ import org.apache.axis.types.URI.MalformedURIException;
 import javax.xml.namespace.QName;
 import org.globus.wsrf.encoding.ObjectSerializer;
 import org.globus.wsrf.encoding.SerializationException;
+
+import edu.northwestern.radiology.aim.Series;
+import edu.northwestern.radiology.aim.Study;
 import edu.wustl.xipHost.caGrid.GridLocation;
 import edu.wustl.xipHost.dataModel.SearchResult;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
+import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
+import gov.nih.nci.cagrid.data.client.DataServiceClient;
+import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
+import gov.nih.nci.cagrid.ncia.client.NCIACoreServiceClient;
+import gov.nih.nci.ncia.domain.Patient;
 
 /**
  * @author Jaroslaw Krych
@@ -38,7 +46,7 @@ public class GridQuery implements Runnable {
 				e.printStackTrace();
 			}		
 			System.out.println(gridLoc.getAddress()); */
-			searchResult = gridMgr.query(cqlQuery, gridLoc);
+			searchResult = query(cqlQuery, gridLoc);
 			fireUpdateUI();
 			//System.out.println("Grid Query number of studies: " + dicomSearchResult.getStudies().size());			
 		} catch (MalformedURIException e) {
@@ -55,6 +63,51 @@ public class GridQuery implements Runnable {
 			return;
 		}		
 	}
+	
+	/* (non-Javadoc)
+	 * @see edu.wustl.xipHost.caGrid.GridManager#query(gov.nih.nci.cagrid.cqlquery.CQLQuery, edu.wustl.xipHost.caGrid.GridLocation)
+	 */
+	public SearchResult query(CQLQuery query, GridLocation location) throws MalformedURIException, RemoteException, ConnectException{		
+		DataServiceClient dicomClient = null;
+		NCIACoreServiceClient nciaClient = null;
+		CQLQueryResultsIterator iter;		
+		if(location != null && location.getProtocolVersion().equalsIgnoreCase("DICOM")){
+			dicomClient = new DataServiceClient(location.getAddress());			
+		}else if(location != null && location.getProtocolVersion().equalsIgnoreCase("NBIA-4.2")){
+			nciaClient = new NCIACoreServiceClient(location.getAddress());
+		}else{
+			return null;
+		}
+		final CQLQuery fcqlq = query;		
+		CQLQueryResults results = null;
+		if(location != null && location.getProtocolVersion().equalsIgnoreCase("DICOM")){
+			results = dicomClient.query(fcqlq);
+		}else if(location != null && location.getProtocolVersion().equalsIgnoreCase("NBIA-4.2")){
+			results = nciaClient.query(fcqlq);
+		}						
+        iter = new CQLQueryResultsIterator(results);        
+        
+        int ii = 0;
+		while (iter.hasNext()) {
+			System.out.println(ii++);
+			java.lang.Object obj = iter.next();
+			if (obj == null) {
+				System.out.println("something not right.  obj is null");
+				continue;
+			}
+			//gov.nih.nci.ncia.domain.Patient patient = gov.nih.nci.ncia.domain.Patient.class.cast(obj);	
+			gov.nih.nci.ncia.domain.Series series = gov.nih.nci.ncia.domain.Series.class.cast(obj);
+			//System.out.println("Patient Id: " + patient.getPatientId());							
+			//System.out.println("Patient name: " + patient.getPatientName());
+			System.out.println("SeriesInstanceUID: " + series.getSeriesInstanceUID());							
+		}
+                
+        SearchResult result = GridUtil.convertCQLQueryResultsIteratorToSearchResult(iter, location);	        
+        return result;			
+	}	
+	
+	
+	
 	
 	public SearchResult getSearchResult(){
 		return searchResult;
