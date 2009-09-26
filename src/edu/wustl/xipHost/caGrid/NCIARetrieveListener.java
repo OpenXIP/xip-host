64faeff63dd01cfc0a1d1ba84c9aeef2b225c6fc
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-09-26 14:05:08 +0000

    Added old NCIARetrieve, NCIARetriveEvent, NCIARetrieveListener to perform retrieve operations based on NCIACoreServiceClient.
    New NCIADataServiceHelper.retrieve(...) does not return results. 
    
    SVN-Revision: 223

---
+++ src/edu/wustl/xipHost/caGrid/NCIARetrieveListener.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2008 Washington University in St. Louis. All Rights Reserved.
+ */
+package edu.wustl.xipHost.caGrid;
+
+/**
+ * @author Jaroslaw Krych
+ *
+ */
+public interface NCIARetrieveListener {
+	public void importedFilesAvailable(NCIARetrieveEvent e);
+}
