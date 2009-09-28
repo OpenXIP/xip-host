57703defd025efad60ff7bc54ada5ba308d5e232
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-09-28 12:12:22 +0000

    AVTStore modified to work only with AIM objects. Part related to AIMs and DICOM segmentation objects was commented.
    SVN-Revision: 234

--- src/edu/wustl/xipHost/avt/AVTStore.java
+++ src/edu/wustl/xipHost/avt/AVTStore.java
@@ -65,6 +65,7 @@ public class AVTStore implements Runnable {
 	
 	boolean bln;
 	public void run() {						
+		/* used with old version of IA and AD
 		if(aimToStore != null && attachmentsToStore != null){
 				File aimFile = aimToStore.get(0);
 				File dicomFile = attachmentsToStore.get(0);			
@@ -78,6 +79,21 @@ public class AVTStore implements Runnable {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+		}*/
+		if(aimToStore != null){
+			for(int i = 0; i < aimToStore.size(); i++){
+				File aimFile = aimToStore.get(i);			
+				try {				
+					ImageAnnotation imageAnnotation = AnnotationIO.loadAnnotationFromFile(aimFile);
+					AnnotationIO.saveOrUpdateAnnotation(imageAnnotation);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JDOMException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}			
 		}
 		notifyStoreResult(true);
 	}
