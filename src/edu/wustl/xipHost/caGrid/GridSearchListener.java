d1aeca3e3a64d08640918ce033dfb05badb4a078
Krych, Jaroslaw <jarek.krych@gmail.com>
2009-11-16 21:58:01 +0000

    Added notifyException method to GridSearchListener interface.
    SVN-Revision: 273

--- src/edu/wustl/xipHost/caGrid/GridSearchListener.java
+++ src/edu/wustl/xipHost/caGrid/GridSearchListener.java
@@ -10,4 +10,5 @@ import java.util.EventListener;
  */
 public interface GridSearchListener extends EventListener {
 	public void searchResultAvailable(GridSearchEvent e);
+	public void notifyException(String message);
 }
