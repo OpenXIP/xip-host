7edbe835ccb6382cd70d4d15177d506dfcc7da6a
Kelsey, Matthew <kelseym@mir.wustl.edu>
2010-07-08 00:05:12 +0000

    Changed subelement to include study/series criteria.  Replaced Vector with List.
    SVN-Revision: 407

--- src/edu/wustl/xipHost/avt2ext/iterator/TargetIterator.java
+++ src/edu/wustl/xipHost/avt2ext/iterator/TargetIterator.java
@@ -3,7 +3,6 @@
  */
 package edu.wustl.xipHost.avt2ext.iterator;
 
-import java.util.Vector;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -22,7 +21,7 @@ import org.dcm4che2.data.Tag;
 public class TargetIterator implements Iterator<TargetElement> {
 	
 	Integer currentElementIndex = -1;
-	Vector<TargetElement> targetElements = new Vector<TargetElement>();
+	List<TargetElement> targetElements = new ArrayList<TargetElement>();
 	SearchResult selectedDataSearchResult;
 	IterationTarget target;
 	
@@ -41,15 +40,19 @@ public class TargetIterator implements Iterator<TargetElement> {
 					if(patient.getLastUpdated() != null) {
 						Criteria patientCriteria = new Criteria(selectedDataSearchResult.getOriginalCriteria().getDICOMCriteria(),
 																selectedDataSearchResult.getOriginalCriteria().getAIMCriteria());
-						patientCriteria.getDICOMCriteria().put(Tag.PatientName, patient.getPatientName());
+						patientCriteria.getDICOMCriteria().put(Tag.PatientID, patient.getPatientID());
 						
-						// Build Criteria for each Patient/Study SubElements
+						// Build Criteria for each Patient/Study/Series SubElement
 						List<SubElement> subElements = new ArrayList<SubElement>();
 						for(Study study : patient.getStudies()) {
 							Criteria studyCriteria = new Criteria(patientCriteria.getDICOMCriteria(), patientCriteria.getAIMCriteria());
 							studyCriteria.getDICOMCriteria().put(Tag.StudyInstanceUID, study.getStudyInstanceUID());
-							SubElement studySubElement = new SubElement(studyCriteria, null);
-							subElements.add(studySubElement);
+							for(Series series : study.getSeries()) {
+								Criteria seriesCriteria = new Criteria(studyCriteria.getDICOMCriteria(), studyCriteria.getAIMCriteria());
+								seriesCriteria.getDICOMCriteria().put(Tag.SeriesInstanceUID, series.getSeriesInstanceUID());
+								SubElement studySubElement = new SubElement(seriesCriteria, null);
+								subElements.add(studySubElement);
+							}
 						}
 						TargetElement element = new TargetElement(patient.getPatientID(), subElements, target);
 						targetElements.add(element);
@@ -60,45 +63,9 @@ public class TargetIterator implements Iterator<TargetElement> {
 				}
 	
 			} else if (target == IterationTarget.STUDY) {
-				for(Patient patient : selectedDataSearchResult.getPatients()) {
-					Criteria patientCriteria = new Criteria(selectedDataSearchResult.getOriginalCriteria().getDICOMCriteria(),
-															selectedDataSearchResult.getOriginalCriteria().getAIMCriteria());
-					patientCriteria.getDICOMCriteria().put(Tag.PatientName, patient.getPatientName());
-					for(Study study : patient.getStudies()) {
-						Criteria studyCriteria = new Criteria(patientCriteria.getDICOMCriteria(), patientCriteria.getAIMCriteria());
-						studyCriteria.getDICOMCriteria().put(Tag.StudyInstanceUID, study.getStudyInstanceUID());
-						
-						// Build Criteria for each Patient/Study/Series SubElements
-						List<SubElement> subElements = new ArrayList<SubElement>();
-						for(Series series : study.getSeries()) {
-							Criteria seriesCriteria = new Criteria(studyCriteria.getDICOMCriteria(), studyCriteria.getAIMCriteria());
-							seriesCriteria.getDICOMCriteria().put(Tag.SeriesInstanceUID, series.getSeriesInstanceUID());
-							SubElement studySubElement = new SubElement(seriesCriteria, null);
-							subElements.add(studySubElement);
-						}
-						TargetElement element = new TargetElement(study.getStudyID(), subElements, target);
-						targetElements.add(element);
-					}
-				}
+				// TODO
 			} else if (target == IterationTarget.SERIES) {
-				for(Patient patient : selectedDataSearchResult.getPatients()) {
-					Criteria patientCriteria = new Criteria(selectedDataSearchResult.getOriginalCriteria().getDICOMCriteria(),
-															selectedDataSearchResult.getOriginalCriteria().getAIMCriteria());
-					patientCriteria.getDICOMCriteria().put(Tag.PatientName, patient.getPatientName());
-					for(Study study : patient.getStudies()) {
-						Criteria studyCriteria = new Criteria(patientCriteria.getDICOMCriteria(), patientCriteria.getAIMCriteria());
-						studyCriteria.getDICOMCriteria().put(Tag.StudyInstanceUID, study.getStudyInstanceUID());
-						for(Series series : study.getSeries()) {
-							Criteria seriesCriteria = new Criteria(studyCriteria.getDICOMCriteria(), studyCriteria.getAIMCriteria());
-							seriesCriteria.getDICOMCriteria().put(Tag.SeriesInstanceUID, series.getSeriesInstanceUID());
-							
-							// No SubElements, 
-							TargetElement element = new TargetElement(series.getSeriesInstanceUID(), null, target);
-							targetElements.add(element);
-						}
-					}
-				}
-			
+				// TODO
 			}
 		} catch(Exception e) {
 			currentElementIndex = -1;
