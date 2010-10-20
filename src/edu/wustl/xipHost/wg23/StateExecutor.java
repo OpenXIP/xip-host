3995674a704a630f63b8c0e811cb6751834d616f
Krych, Jaroslaw <jarek.krych@gmail.com>
2010-10-20 13:42:12 +0000

    Modified StateExecutor to be used to asynchronously notify hosted application about state changes of an application on the host site.
    SVN-Revision: 510

--- src/edu/wustl/xipHost/wg23/StateExecutor.java
+++ src/edu/wustl/xipHost/wg23/StateExecutor.java
@@ -1,21 +1,28 @@
 /**
- * Copyright (c) 2008 Washington University in Saint Louis. All Rights Reserved.
+ * Copyright (c) 2010 Washington University in St. Louis. All Rights Reserved.
  */
 package edu.wustl.xipHost.wg23;
+
+import org.nema.dicom.wg23.State;
+import edu.wustl.xipHost.application.Application;
 
 /**
  * @author Jaroslaw Krych
  *
  */
 public class StateExecutor implements Runnable{
-	HostImpl hostImpl;
+	Application application;
+	State state;
 	
-	public StateExecutor(HostImpl hostImpl){
-		this.hostImpl = hostImpl;
+	public StateExecutor(Application application){
+		this.application = application;
+	}
+	
+	public void setState(State state){
+		this.state = state;
 	}
 	
 	public void run() {
-		hostImpl.fireStateChangedAction();		
+		application.getClientToApplication().setState(state);		
 	}
-
 }
