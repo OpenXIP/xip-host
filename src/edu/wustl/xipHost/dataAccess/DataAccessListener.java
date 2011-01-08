68ad8835b08ef75277d127aeb744e7e2cfb0c1fa
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-01-08 16:22:15 +0000

    Added notifyException(String message) method to DataAccessListener interface.
    SVN-Revision: 701

--- src/edu/wustl/xipHost/dataAccess/DataAccessListener.java
+++ src/edu/wustl/xipHost/dataAccess/DataAccessListener.java
@@ -12,4 +12,5 @@ import java.util.EventListener;
 public interface DataAccessListener extends EventListener{
 	public void queryResultsAvailable(QueryEvent e);
 	public void retriveResultsAvailable(RetrieveEvent e);
+	public void notifyException(String message);
 }
