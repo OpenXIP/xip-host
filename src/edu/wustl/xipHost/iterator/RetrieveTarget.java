4812950ae3ef68c57355129225d6e5eb29c912ad
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-01-10 16:49:57 +0000

    Added RetrieveTarget to the iterator package. The RetrieveTaret replaced old ADRetrieveTarget.
    SVN-Revision: 716

---
+++ src/edu/wustl/xipHost/iterator/RetrieveTarget.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2010 Washington University in St. Louis. All Rights Reserved.
+ */
+package edu.wustl.xipHost.iterator;
+
+/**
+ * @author Jaroslaw Krych
+ *
+ */
+public enum RetrieveTarget {
+	SERIES, DICOM_AND_AIM, AIM_SEG
+}
