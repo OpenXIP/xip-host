c8a0c45442c64ee93133618a51b7120abaa6e29f
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-01-15 10:33:55 +0000

    Modified run() of the DicomRetrieve. Added retrieve(TargetElement targetElement, RetrieveTarget retrieveTarget) method to be used when retrieving data based on the target elements. Added logging statements.
    SVN-Revision: 744

--- src/edu/wustl/xipHost/dicom/DicomRetrieve.java
+++ src/edu/wustl/xipHost/dicom/DicomRetrieve.java
@@ -9,15 +9,16 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import org.apache.log4j.Logger;
 import com.pixelmed.database.StudySeriesInstanceModel;
 import com.pixelmed.dicom.Attribute;
 import com.pixelmed.dicom.AttributeList;
 import com.pixelmed.dicom.AttributeTag;
+import com.pixelmed.dicom.CodeStringAttribute;
+import com.pixelmed.dicom.DicomDictionary;
 import com.pixelmed.dicom.DicomException;
-import com.pixelmed.dicom.PersonNameAttribute;
 import com.pixelmed.dicom.SetOfDicomFiles;
-import com.pixelmed.dicom.ShortStringAttribute;
-import com.pixelmed.dicom.SpecificCharacterSet;
 import com.pixelmed.dicom.TagFromName;
 import com.pixelmed.dicom.UniqueIdentifierAttribute;
 import com.pixelmed.network.DicomNetworkException;
@@ -30,6 +31,7 @@ import edu.wustl.xipHost.dataAccess.DataAccessListener;
 import edu.wustl.xipHost.dataAccess.Retrieve;
 import edu.wustl.xipHost.dataAccess.RetrieveEvent;
 import edu.wustl.xipHost.iterator.RetrieveTarget;
+import edu.wustl.xipHost.iterator.SubElement;
 import edu.wustl.xipHost.iterator.TargetElement;
 
 /**
@@ -37,6 +39,7 @@ import edu.wustl.xipHost.iterator.TargetElement;
  *
  */
 public class DicomRetrieve implements Retrieve {
+	final static Logger logger = Logger.getLogger(DicomRetrieve.class);	
 	DicomManager dicomMgr;
 	AttributeList criteria;
 	PacsLocation called;
@@ -58,50 +61,15 @@ public class DicomRetrieve implements Retrieve {
 	@Override
 	public void setRetrieve(TargetElement targetElement, RetrieveTarget retrieveTarget) {
 		this.targetElement = targetElement;
-		/*
-		List<SubElement> subElements = targetElement.getSubElements();
-		for(SubElement subElement : subElements){
-			Map<Integer, Object> dicomCriteria = subElement.getCriteria().getDICOMCriteria();
-			Iterator<Integer> iter = dicomCriteria.keySet().iterator();
-			while(iter.hasNext()){
-				Integer key = iter.next();
-				Object value = dicomCriteria.get(key);
-			}
-			
-		}
-		*/
-		
-		
-		AttributeList filter = new AttributeList();
-		
-			String[] characterSets = { "ISO_IR 100" };
-			SpecificCharacterSet specificCharacterSet = new SpecificCharacterSet(characterSets);
-			try{
-				{ AttributeTag t = TagFromName.PatientID; Attribute a = new ShortStringAttribute(t,specificCharacterSet); a.addValue("1.3.6.1.4.1.9328.50.1.0022"); filter.put(t,a); }
-				{ AttributeTag t = TagFromName.StudyInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue("1.3.6.1.4.1.9328.50.1.10607"); filter.put(t,a); }
-				{ AttributeTag t = TagFromName.SeriesInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue("1.3.6.1.4.1.9328.50.1.10697"); filter.put(t,a); }
-			} catch (Exception e) {
-				e.printStackTrace(System.err);			
-			}
-		this.criteria = filter;	
 		this.retrieveTarget = retrieveTarget; 
 	}
 
 	
 	List<File> files = new ArrayList<File>();
 	public void run() {
-		if(criteria == null){return;}					
-		if(criteria != null && called != null && calling != null){													
-			List<URI> uris = retrieve(criteria, called, calling);
-			for(int i = 0; i < uris.size(); i++){
-				File file = new File(uris.get(i));
-				files.add(file);				
-			}
-
-			fireResultsAvailable(targetElement.getId());		
-		}else{
-			return;
-		}				
+		logger.info("Executing DICOM retrieve.");				
+		retrieve(targetElement, retrieveTarget);
+		fireResultsAvailable(targetElement.getId());						
 	}
 	
 	List<URI> retrievedFiles;
@@ -109,14 +77,33 @@ public class DicomRetrieve implements Retrieve {
 	 * 1. Method performed hierarchical move from remote location to calling location first.
 	 * 2. To get retrieved files' URIs method performs query but on the local server (calling)
 	 */
-	public List<URI> retrieve(AttributeList criteria, PacsLocation called, PacsLocation calling) {
+	public List<URI> retrieve(AttributeList criteria, PacsLocation called, PacsLocation calling) {		
 		retrievedFiles = new ArrayList<URI>();	    	    		    	    		        
         try {
         	String hostName = called.getAddress();
+        	logger.debug("Host name: " + hostName);
         	int port = called.getPort();
-        	String calledAETitle = called.getAETitle(); 
+        	logger.debug("Port: " + port);
+        	String calledAETitle = called.getAETitle();
+        	logger.debug("CalledAETitle: " + calledAETitle);
         	String callingAETitle = calling.getAETitle();
+        	logger.debug("CallingAETitle: " + callingAETitle);
         	StudyRootQueryInformationModel mModel = new StudyRootQueryInformationModel(hostName, port, calledAETitle, callingAETitle, 0);
+        	logger.debug("DICOM retrieve criteria");
+        	if(logger.isDebugEnabled()){
+        		DicomDictionary dictionary = AttributeList.getDictionary();
+    		    Iterator<?> iter = dictionary.getTagIterator();        
+    		    String strAtt = null;
+    		    String attValue = null;
+    		    while(iter.hasNext()){
+    		    	AttributeTag attTag  = (AttributeTag)iter.next();
+    				strAtt = attTag.toString();									
+    				attValue = Attribute.getSingleStringValueOrEmptyString(criteria, attTag);
+    				if(!attValue.isEmpty()){
+    					logger.debug(strAtt + " " + attValue);				
+    				}
+    		    }	
+        	}
         	mModel.performHierarchicalMoveTo(criteria, calling.getAETitle());			
 		} catch (IOException e1) {
 			return null;
@@ -148,9 +135,85 @@ public class DicomRetrieve implements Retrieve {
 				localResults = mQueryResponseGenerator.next();
 			}			
     	} catch (Exception e) {
+    		logger.error(e, e);
     		return null;
     	}      	    	    	
     	return retrievedFiles;
+	}
+	
+	void retrieve(TargetElement targetElement, RetrieveTarget retrieveTarget){		
+		retrievedFiles = new ArrayList<URI>();
+		String hostName = called.getAddress();
+		int port = called.getPort();
+		String calledAETitle = called.getAETitle();
+		String callingAETitle = calling.getAETitle();
+		if(called != null && calling != null){	
+			if(logger.isDebugEnabled()){			
+		    	logger.debug("Host name: " + hostName);	    	
+		    	logger.debug("Port: " + port);	    
+		    	logger.debug("CalledAETitle: " + calledAETitle);	    	
+		    	logger.debug("CallingAETitle: " + callingAETitle);
+			}		
+			List<SubElement> subElements = targetElement.getSubElements();
+			for(SubElement subElement : subElements){
+				Map<Integer, Object> dicomCriteria = subElement.getCriteria().getDICOMCriteria();
+				Object valueStudyInstanceUID = dicomCriteria.get(new Integer(2097165));	//studyInstanceUID
+				Object valueSeriesInstanceUID = dicomCriteria.get(new Integer(2097166));	//seriesInstanceUID
+				try{
+					{ AttributeTag t = TagFromName.StudyInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue(valueStudyInstanceUID.toString()); criteria.put(t,a); }
+					{ AttributeTag t = TagFromName.SeriesInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue(valueSeriesInstanceUID.toString()); criteria.put(t,a); }
+					{ AttributeTag t = TagFromName.SOPInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue("*"); criteria.put(t,a); }
+					{ AttributeTag t = TagFromName.QueryRetrieveLevel; Attribute a = new CodeStringAttribute(t); a.addValue("IMAGE"); criteria.put(t,a); }
+				} catch (Exception e) {
+					e.printStackTrace(System.err);			
+				}
+				if(criteria == null){
+					logger.warn("DICOM criteria NULL");
+					return;
+				}					
+				Iterator<Integer> iter = dicomCriteria.keySet().iterator();
+				logger.debug("DICOM retrieve criteria");
+				while(iter.hasNext()){
+					Integer key = iter.next();
+					Object value = dicomCriteria.get(key);
+					logger.debug("Key: " + key + " Value: " +  value);
+				}			    	    		    	    		        
+		        try {	        	
+		        	StudyRootQueryInformationModel mModel = new StudyRootQueryInformationModel(hostName, port, calledAETitle, callingAETitle, 0);	        		        	
+		        	mModel.performHierarchicalMoveTo(criteria, calling.getAETitle());			
+				} catch (IOException e) {
+					logger.error(e, e);
+				} catch (DicomException e) {
+					logger.error(e, e);
+				} catch (DicomNetworkException e) {						
+					logger.error(e, e);
+				}        	        	    	    						
+				try {						    		
+					//System.out.println("Local server is: " + getDBFileName());
+					StudySeriesInstanceModel mDatabase = new StudySeriesInstanceModel(dicomMgr.getDBFileName());			
+		    		//RetrieveResposeGeneratorFactory provides access to files URLs stored in hsqldb    		    	
+		    		RetrieveResponseGeneratorFactory mRetrieveResponseGeneratorFactory = mDatabase.getRetrieveResponseGeneratorFactory(0);
+		    		QueryResponseGeneratorFactory mQueryResponseGeneratorFactory = mDatabase.getQueryResponseGeneratorFactory(0);			
+		    		RetrieveResponseGenerator mRetrieveResponseGenerator = mRetrieveResponseGeneratorFactory.newInstance();
+					QueryResponseGenerator mQueryResponseGenerator = mQueryResponseGeneratorFactory.newInstance();						
+					mQueryResponseGenerator.performQuery("1.2.840.10008.5.1.4.1.2.2.1", criteria, true);	// Study Root						
+					AttributeList localResults = mQueryResponseGenerator.next();			
+					while(localResults != null) {							 					
+						mRetrieveResponseGenerator.performRetrieve("1.2.840.10008.5.1.4.1.2.2.3", localResults, true);	// Study Root		
+						SetOfDicomFiles dicomFiles = mRetrieveResponseGenerator.getDicomFiles();
+						Iterator<?> it = dicomFiles.iterator();			  
+						while (it.hasNext() ) {
+							SetOfDicomFiles.DicomFile x  = (SetOfDicomFiles.DicomFile)it.next();
+							logger.debug("Dicom file: " + x.getFileName());			    
+							retrievedFiles.add(new File(x.getFileName()).toURI());
+						}		
+						localResults = mQueryResponseGenerator.next();
+					}			
+		    	} catch (Exception e) {
+		    		logger.error(e, e);
+		    	}      	    	    	
+			}		
+		}		
 	}
 	
 	public List<File> getRetrievedFiles(){
