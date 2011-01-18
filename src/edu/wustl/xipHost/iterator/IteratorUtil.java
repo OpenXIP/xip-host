7dab1bcbb7a127cdb83521d6a2ba85e9acac8e1f
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-01-18 22:12:00 +0000

    Added new getAvailableData() method used to convert TargetElement into WG23 AvailableData object.
    SVN-Revision: 773

--- src/edu/wustl/xipHost/iterator/IteratorUtil.java
+++ src/edu/wustl/xipHost/iterator/IteratorUtil.java
@@ -5,7 +5,6 @@ package edu.wustl.xipHost.iterator;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -26,6 +25,7 @@ import org.nema.dicom.wg23.ObjectLocator;
 import org.nema.dicom.wg23.Uid;
 import org.nema.dicom.wg23.Uuid;
 
+import edu.wustl.xipHost.dataModel.Item;
 import edu.wustl.xipHost.dataModel.Patient;
 import edu.wustl.xipHost.dataModel.SearchResult;
 import edu.wustl.xipHost.dataModel.Series;
@@ -274,6 +274,71 @@ public class IteratorUtil {
 		dataModel.setObjectLocators(objLocs);
 		WG23DataModel wg23DataModel = dataModel;		
 		return wg23DataModel;
+	}
+	
+	public static synchronized AvailableData getAvailableData(TargetElement targetElement){
+		SearchResult subSearchResult = targetElement.getSubSearchResult();
+		AvailableData availableData = new AvailableData();		
+		ArrayOfPatient arrayOfPatient = new ArrayOfPatient();
+		List<org.nema.dicom.wg23.Patient> listOfPatients = arrayOfPatient.getPatient();
+		List<Patient> searchResultPatients = subSearchResult.getPatients();
+		for(int i = 0; i < searchResultPatients.size(); i++){
+			Patient searchResultPatient = searchResultPatients.get(i);
+			String patientName = searchResultPatient.getPatientName();			
+			org.nema.dicom.wg23.Patient patient = new org.nema.dicom.wg23.Patient();
+			patient.setName(patientName);
+			ArrayOfObjectDescriptor arrayOfObjectDescPatient = new ArrayOfObjectDescriptor();
+			List<ObjectDescriptor> listObjectDescsPatient = arrayOfObjectDescPatient.getObjectDescriptor();
+			List<Item> patientItems = searchResultPatient.getItems();
+			for(Item item : patientItems){
+				ObjectDescriptor objDesc = item.getObjectDescriptor();
+				listObjectDescsPatient.add(objDesc);
+			}
+			patient.setObjectDescriptors(arrayOfObjectDescPatient);
+			listOfPatients.add(patient);
+			List<Study> searchResultStudies = searchResultPatient.getStudies();
+			ArrayOfStudy arrayOfStudy = new ArrayOfStudy();
+			List<org.nema.dicom.wg23.Study> listOfStudies = arrayOfStudy.getStudy();
+			for(int j = 0; j < searchResultStudies.size(); j++){
+				Study searchResultStudy = searchResultStudies.get(j);							
+				String studyInstanceUID = searchResultStudy.getStudyInstanceUID();
+				org.nema.dicom.wg23.Study study = new org.nema.dicom.wg23.Study();
+				study.setStudyUID(studyInstanceUID);
+				ArrayOfObjectDescriptor arrayOfObjectDescStudy = new ArrayOfObjectDescriptor();
+				List<ObjectDescriptor> listObjectDescsStudy = arrayOfObjectDescStudy.getObjectDescriptor();
+				List<Item> studyItems = searchResultStudy.getItems();
+				for(Item item : studyItems){
+					ObjectDescriptor objDesc = item.getObjectDescriptor();
+					listObjectDescsStudy.add(objDesc);
+				}
+				study.setObjectDescriptors(arrayOfObjectDescStudy);
+				listOfStudies.add(study);
+				patient.setStudies(arrayOfStudy);
+				List<Series> searchResultSeries = searchResultStudy.getSeries();
+				ArrayOfSeries arrayOfSeries = new ArrayOfSeries();
+				List<org.nema.dicom.wg23.Series> listOfSeries = arrayOfSeries.getSeries();
+				for(int k = 0; k < searchResultSeries.size(); k++){
+					Series oneSeries = searchResultSeries.get(k);
+					String seriesInstanceUID = oneSeries.getSeriesInstanceUID();
+					org.nema.dicom.wg23.Series series = new org.nema.dicom.wg23.Series();
+					series.setSeriesUID(seriesInstanceUID);
+					ArrayOfObjectDescriptor arrayOfObjectDescSeries = new ArrayOfObjectDescriptor();
+					List<ObjectDescriptor> listObjectDescsSeries = arrayOfObjectDescSeries.getObjectDescriptor();
+					List<Item> seriesItems = oneSeries.getItems();
+					for(Item item : seriesItems){
+						ObjectDescriptor objDesc = item.getObjectDescriptor();
+						listObjectDescsSeries.add(objDesc);
+					}
+					series.setObjectDescriptors(arrayOfObjectDescSeries);
+					listOfSeries.add(series);
+					study.setSeries(arrayOfSeries);
+				}
+			}
+			availableData.setPatients(arrayOfPatient);
+			ArrayOfObjectDescriptor arrayOfObjectDescTopLevel = new ArrayOfObjectDescriptor();					 					
+			availableData.setObjectDescriptors(arrayOfObjectDescTopLevel);
+		}
+		return availableData;
 	}
 	
 	public static int getTotalNumberOfItems(SearchResult subSearchResult){
