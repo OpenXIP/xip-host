6e06cea2c78e6e906054ae2c0e32f4879481eda7
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-01-18 22:09:25 +0000

    Removed unused dicomFileURIs variable from SubElement class.
    SVN-Revision: 772

--- src/edu/wustl/xipHost/iterator/SubElement.java
+++ src/edu/wustl/xipHost/iterator/SubElement.java
@@ -3,9 +3,6 @@
  */
 package edu.wustl.xipHost.iterator;
 
-import java.net.URI;
-import java.util.List;
-
 /**
  * @author Jaroslaw Krych
  *
@@ -13,7 +10,6 @@ import java.util.List;
 public class SubElement {
 	Criteria criteria;
 	String path;
-	List<URI> dicomFileURIs;
 	
 	public SubElement(Criteria criteria, String path) {
 		super();
@@ -31,13 +27,5 @@ public class SubElement {
 	
 	public void setPath(String strPath){
 		path = strPath;
-	}
-	
-	public List<URI> getFileURIs(){
-		return dicomFileURIs;
-	}
-	
-	public void setFileURIs(List<URI> dicomFileURIs){
-		this.dicomFileURIs = dicomFileURIs;
 	}
 }
