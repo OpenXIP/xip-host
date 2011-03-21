eeabbd0e5f91444f9e832326f4647ac06c62028e
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-03-21 15:36:25 +0000

    Modified RetrieveFromPacsLocationTest JUnit test. Modifications are not final and unit test doesn't assert the processing result at the time of this commit.
    SVN-Revision: 854

--- src-tests/edu/wustl/xipHost/dicom/RetrieveFromPacsLocationTest.java
+++ src-tests/edu/wustl/xipHost/dicom/RetrieveFromPacsLocationTest.java
@@ -1,10 +1,17 @@
 /**
- * Copyright (c) 2008 Washington University in Saint Louis. All Rights Reserved.
+ * Copyright (c) 2011 Washington University in St. Louis. All Rights Reserved.
  */
 package edu.wustl.xipHost.dicom;
 
-import java.net.URI;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.nema.dicom.wg23.ObjectLocator;
 import com.pixelmed.dicom.Attribute;
 import com.pixelmed.dicom.AttributeList;
 import com.pixelmed.dicom.AttributeTag;
@@ -12,9 +19,24 @@ import com.pixelmed.dicom.CodeStringAttribute;
 import com.pixelmed.dicom.DicomException;
 import com.pixelmed.dicom.TagFromName;
 import com.pixelmed.dicom.UniqueIdentifierAttribute;
-
+import edu.wustl.xipHost.dataAccess.DataAccessListener;
+import edu.wustl.xipHost.dataAccess.Query;
+import edu.wustl.xipHost.dataAccess.QueryEvent;
+import edu.wustl.xipHost.dataAccess.RetrieveEvent;
+import edu.wustl.xipHost.dataModel.Patient;
+import edu.wustl.xipHost.dataModel.SearchResult;
+import edu.wustl.xipHost.dataModel.Series;
+import edu.wustl.xipHost.dataModel.Study;
 import edu.wustl.xipHost.dicom.server.Workstation2;
 import edu.wustl.xipHost.dicom.server.Workstation3;
+import edu.wustl.xipHost.iterator.Criteria;
+import edu.wustl.xipHost.iterator.IterationTarget;
+import edu.wustl.xipHost.iterator.IteratorElementEvent;
+import edu.wustl.xipHost.iterator.IteratorEvent;
+import edu.wustl.xipHost.iterator.RetrieveTarget;
+import edu.wustl.xipHost.iterator.TargetElement;
+import edu.wustl.xipHost.iterator.TargetIteratorListener;
+import edu.wustl.xipHost.iterator.TargetIteratorRunner;
 
 import junit.framework.TestCase;
 
@@ -22,16 +44,17 @@ import junit.framework.TestCase;
  * @author Jaroslaw Krych
  *
  */
-public class RetrieveFromPacsLocationTest extends TestCase {
-	AttributeList criteria;	
+public class RetrieveFromPacsLocationTest extends TestCase implements DataAccessListener, TargetIteratorListener {
+	final static Logger logger = Logger.getLogger(RetrieveFromPacsLocationTest.class);	
 	PacsLocation calling;
 	PacsLocation called;
 	DicomManager dicomMgr;
 	AttributeList retrieveCriteria;
+	DicomRetrieve dicomRetrieve;
 	protected void setUp(){
 		called = new PacsLocation("127.0.0.1", 3002, "WORKSTATION2", "WashU WS2");		
 		calling = new PacsLocation("127.0.0.1", 3003, "WORKSTATION3", "WashU WS3");
-		retrieveCriteria = new AttributeList();	
+		retrieveCriteria = DicomUtil.constructEmptyAttributeList();
 		try{
 			{ AttributeTag t = TagFromName.StudyInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue("2.16.840.1.113669.6.4.0.1152905158098.2006284135300993"); retrieveCriteria.put(t,a); }			
 			{ AttributeTag t = TagFromName.SeriesInstanceUID; Attribute a = new UniqueIdentifierAttribute(t); a.addValue("2.16.840.1.113662.4.2724971601059.1160591040.79352554742476"); retrieveCriteria.put(t,a); }
@@ -52,14 +75,105 @@ public class RetrieveFromPacsLocationTest extends TestCase {
 		Workstation3.stopHSQLDB();
 	}
 	
+	boolean retrieveResultAvailable = false;
+	
 //	DicomManagerImpl 1A - basic flow. AttributeList, PacsLocation are valid and network is on.
 	public void testRetrieveFromPacsLocation1A() {	
-		System.out.println(retrieveCriteria.toString());
-		/*
-		List<URI> uris = retrieve(retrieveCriteria, called, calling);
-		for(int i = 0; i < uris.size(); i++){
-			System.out.println(uris.get(i));
+		logger.debug(retrieveCriteria.toString());
+		dicomRetrieve = new DicomRetrieve(retrieveCriteria, called, calling);
+		SearchResult selectedDataSearchResult = new SearchResult();
+		selectedDataSearchResult.setDataSourceDescription("Test data source");
+		Map<Integer, Object> dicomCriteria = DicomUtil.convertToADDicomCriteria(retrieveCriteria);
+		Map<String, Object> aimCriteria = new HashMap<String, Object>();
+		Criteria originalCriteria = new Criteria(dicomCriteria, aimCriteria);
+		selectedDataSearchResult.setOriginalCriteria(originalCriteria);
+		Patient patient = new Patient("Clunie^David", "2037316", "19601109");
+		Timestamp lastUpdated = new Timestamp(Calendar.getInstance().getTime().getTime());
+		patient.setLastUpdated(lastUpdated);
+		selectedDataSearchResult.addPatient(patient);
+		Study study = new Study("20061011", "29206", "Brain", "2.16.840.1.113669.6.4.0.1152905158098.2006284135300993");
+		study.setLastUpdated(lastUpdated);
+		patient.addStudy(study);
+		Series series = new Series("2.16.840.1.113662.4.2724971601059.1160591040.79352554742476", "MR", "", "2.16.840.1.113662.4.2724971601059.1160591040.79352554742476");
+		//series.setLastUpdated(lastUpdated);
+		study.addSeries(series);
+		IterationTarget interTarget = IterationTarget.SERIES;
+		Query query = new DicomQuery();
+		TargetIteratorRunner iterRunner = new TargetIteratorRunner(selectedDataSearchResult, interTarget, query, this);
+		try {
+			Thread t = new Thread(iterRunner);
+			t.start();
+		} catch(Exception e) {
+			logger.error(e, e);
+		}
+		
+		RetrieveTarget retrieveTarget = RetrieveTarget.DICOM_AND_AIM;
+		synchronized(targetElements){
+			while(targetElements.size() == 0){
+				try {
+					targetElements.wait();
+				} catch (InterruptedException e) {
+					logger.error(e, e);
+				}
+			}
+		}
+		//TODO create targetElement
+		TargetElement targetElement = targetElements.get(0);
+		dicomRetrieve.setRetrieve(targetElement, retrieveTarget);
+		dicomRetrieve.addDataAccessListener(this);
+		Thread t = new Thread(dicomRetrieve);
+		t.start();
+		synchronized(this){
+			while(retrieveResultAvailable == false){
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					logger.error(e, e);
+				}
+			}
+		}
+		for(ObjectLocator objLoc : objLocators){
+			logger.debug(objLoc.getUri());
 		}		
-		assertTrue(uris.size() > 0);*/
+		assertTrue(objLocators.size() > 0);
+	}
+
+	@Override
+	public void notifyException(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void queryResultsAvailable(QueryEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	List<ObjectLocator> objLocators = null;
+	@SuppressWarnings("unchecked")
+	@Override
+	public void retrieveResultsAvailable(RetrieveEvent e) {
+		retrieveResultAvailable = true;				
+		objLocators = (List<ObjectLocator>) dicomRetrieve.getObjectLocators();
+		this.notify();
+	}
+
+	@Override
+	public void fullIteratorAvailable(IteratorEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	List<TargetElement> targetElements = new ArrayList<TargetElement>();
+	@Override
+	public void targetElementAvailable(IteratorElementEvent e) {
+		synchronized(targetElements){
+			TargetElement element = (TargetElement) e.getSource();
+			logger.debug("TargetElement available. ID: " + element.getId() + " at time " + System.currentTimeMillis());
+			targetElements.add(element);
+			targetElements.notify();
+		}
+		
 	}
 }
