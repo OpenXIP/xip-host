73aa6e81ba815de0cea983e4a912693bfd62acc0
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-03-29 21:18:31 +0000

    Added new methods to Retrieve interface e.g. setCriteria() x 2, setObjectDescriptors, setImportDir(), other.
    SVN-Revision: 861

--- src/edu/wustl/xipHost/dataAccess/Retrieve.java
+++ src/edu/wustl/xipHost/dataAccess/Retrieve.java
@@ -3,11 +3,13 @@
  */
 package edu.wustl.xipHost.dataAccess;
 
+import java.io.File;
+import java.util.List;
 import java.util.Map;
 
+import org.nema.dicom.wg23.ObjectDescriptor;
 import org.nema.dicom.wg23.ObjectLocator;
 
-import edu.wustl.xipHost.iterator.RetrieveTarget;
 import edu.wustl.xipHost.iterator.TargetElement;
 
 /**
@@ -15,6 +17,15 @@ import edu.wustl.xipHost.iterator.TargetElement;
  *
  */
 public interface Retrieve extends Runnable {
+	public void setCriteria(Map<Integer, Object> dicomCriteria, Map<String, Object> aimCriteria);
+	public void setCriteria(Object criteria);
+	public void setObjectDescriptors(List<ObjectDescriptor> objectDescriptors);
+	public void setImportDir(File importDir);
+	public void setRetrieveTarget(RetrieveTarget retrieveTarget);
+	public void setDataSource(DataSource dataSource);
+	public void addRetrieveListener(RetrieveListener l);
+
+	
 	public void addDataAccessListener(DataAccessListener l);
 	public void setRetrieve(TargetElement targetElement, RetrieveTarget retrieveTarget);
 	public Map<String, ObjectLocator> getObjectLocators();
