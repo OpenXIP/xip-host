5959dde486c172df804886c2cce7058c945ba6c7
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-03-29 21:19:48 +0000

    Added new RetrieveEvent constructor accepting Map containing UUIDs and ObjectLocators instead targetElement.
    SVN-Revision: 862

--- src/edu/wustl/xipHost/dataAccess/RetrieveEvent.java
+++ src/edu/wustl/xipHost/dataAccess/RetrieveEvent.java
@@ -4,12 +4,20 @@
 package edu.wustl.xipHost.dataAccess;
 
 import java.util.EventObject;
+import java.util.Map;
+
+import org.nema.dicom.wg23.ObjectLocator;
 /**
  * @author Jaroslaw Krych
  *
  */
 public class RetrieveEvent extends EventObject {
+	//TODO to be removed
 	public RetrieveEvent(String targetElementID){	
 		super(targetElementID);
+	}
+	
+	public RetrieveEvent(Map<String, ObjectLocator> objectLocators){
+		super(objectLocators);
 	}
 }
