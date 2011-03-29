1137d0a32e978bf577512abdda258d223ae62e98
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-03-29 21:17:30 +0000

    Added new QueryListener to dataAccess package.
    SVN-Revision: 860

---
+++ src/edu/wustl/xipHost/dataAccess/QueryListener.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2011 Washington University in St. Louis. All Rights Reserved.
+ */
+package edu.wustl.xipHost.dataAccess;
+
+import java.util.EventListener;
+
+/**
+ * @author Jaroslaw Krych
+ *
+ */
+public interface QueryListener extends EventListener {
+	public void queryResultsAvailable(QueryEvent e);
+}
