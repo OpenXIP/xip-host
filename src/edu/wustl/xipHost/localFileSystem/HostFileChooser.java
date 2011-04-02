5e78579e3c0e27a3e3e0309af6add3c443db2f3a
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-04-02 13:24:13 +0000

    Voided logic of capturing selected files from HostFileChooser. This logic was moved to new LocalFileSystemPanel.
    SVN-Revision: 885

--- src/edu/wustl/xipHost/localFileSystem/HostFileChooser.java
+++ src/edu/wustl/xipHost/localFileSystem/HostFileChooser.java
@@ -4,6 +4,8 @@
 package edu.wustl.xipHost.localFileSystem;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -11,7 +13,7 @@ import javax.swing.JFrame;
 /**
  * Used to choose files from the local file system
  */
-public class HostFileChooser extends JFileChooser{	
+public class HostFileChooser extends JFileChooser {	
 	File [] selectedFiles;
 	Color xipColor = new Color(51, 51, 102);
 	Color xipBtn = new Color(56, 73, 150);
@@ -21,6 +23,7 @@ public class HostFileChooser extends JFileChooser{
 		setBackground(xipColor);		
 		setMultiSelectionEnabled(multiSelection);		
 		setCurrentDirectory(currentDir);		
+		/*
 		int result = showOpenDialog(new JFrame());	      	    
 	    switch (result) {
 	    case JFileChooser.APPROVE_OPTION:	    	
@@ -30,7 +33,7 @@ public class HostFileChooser extends JFileChooser{
 	    	break;
 	    case JFileChooser.ERROR_OPTION:
 	    	break;	    
-		}
+		}*/
 	}
 	
 	public File[] getSelectedItems(){
