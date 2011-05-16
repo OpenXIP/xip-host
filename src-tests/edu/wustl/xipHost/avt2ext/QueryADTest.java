e94bb268de0d2b7abf126eddd2147c38759a607b
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-05-16 11:39:09 +0000

    Modified QueryADTest JUnit test. Converted test to JUnit 4, added logging. All test succeed when AD DB is preloaded.
    SVN-Revision: 921

--- src-tests/edu/wustl/xipHost/avt2ext/QueryADTest.java
+++ src-tests/edu/wustl/xipHost/avt2ext/QueryADTest.java
@@ -3,142 +3,132 @@
  */
 package edu.wustl.xipHost.avt2ext;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import com.pixelmed.dicom.AttributeList;
 import com.siemens.scr.avt.ad.api.ADFacade;
 import com.siemens.scr.avt.ad.dicom.GeneralStudy;
 import edu.wustl.xipHost.avt2ext.AVTFactory;
 import edu.wustl.xipHost.avt2ext.AVTQuery;
 import edu.wustl.xipHost.dataAccess.DataAccessListener;
+import edu.wustl.xipHost.dataAccess.Query;
 import edu.wustl.xipHost.dataAccess.QueryEvent;
 import edu.wustl.xipHost.dataAccess.QueryTarget;
 import edu.wustl.xipHost.dataAccess.RetrieveEvent;
 import edu.wustl.xipHost.dataModel.Patient;
 import edu.wustl.xipHost.dataModel.SearchResult;
 import edu.wustl.xipHost.dicom.DicomUtil;
-import junit.framework.TestCase;
 
 /**
  * @author Jaroslaw Krych
  *
  */
-public class QueryADTest extends TestCase implements DataAccessListener{
-	ADFacade adService;
-	CriteriaSetup setup;
-	/**
-	 * @param name
-	 */
-	public QueryADTest(String name) {
-		super(name);
-	}
+public class QueryADTest implements DataAccessListener{
+	final static Logger logger = Logger.getLogger(QueryADTest.class);
+	static ADFacade adService;
+	static CriteriaSetup setup;
+	static AttributeList attList;
+	
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
+	@BeforeClass
+	public static void setUp() throws Exception {
 		adService = AVTFactory.getADServiceInstance();
 		setup = new CriteriaSetup();
+		attList = setup.getCriteria();
+		DOMConfigurator.configure("log4j.xml");
 	}
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	@After
+	public void tearDown() throws Exception {
 		result = null;
 	}
 	
-	//AVTQuery - query AD database - basic flow. 
+	//AVTQuery - query AD database - basic flow (Patient level query test).
 	//Parameters: adCriteria, adAimCriteria, ADQueryTarget, previousSearchResult, queryObject are all correct.
-	public void testQueryAD_1A(){
-		/*Map<String, Object> aimCriteria = new HashMap<String, Object>();
-		dicomCriteria.put(Tag.PatientSex, "M");			
-		String key = "Patient.sex";
-		Object value = "M";
-		aimCriteria.put(key, value);
-		List<String> results = adService.findAnnotations(dicomCriteria, aimCriteria);*/
-		AttributeList attList = setup.getCriteria();					
+	@Test
+	public void testQueryAD_1A(){				
 		Map<Integer, Object> adCriteria = DicomUtil.convertToADDicomCriteria(attList);
 		Map<String, Object> adAimCriteria = new HashMap<String, Object>();
-		AVTQuery avtQuery = new AVTQuery(adCriteria, adAimCriteria, QueryTarget.PATIENT, null, null);
+		Query avtQuery = new AVTQuery(adCriteria, adAimCriteria, QueryTarget.PATIENT, null, null);
 		avtQuery.addDataAccessListener(this);
 		Thread t = new Thread(avtQuery);
 		t.start();	
 		try {
 			t.join();
 		} catch (InterruptedException e) {			
-			e.printStackTrace();
+			logger.error(e, e);
 		}
 		String patientID = "YAMAMOTO-00046";
 		boolean isQueryOK = result.contains(patientID);
 		boolean isNumPatients = (result.getPatients().size() == 1);
-		assertTrue("The number of patients in AD > 0 but system unable to find data.", isQueryOK);
+		assertTrue("Unable to find specified patientID.", isQueryOK);
 		assertTrue("Actual number of found patients is different than 1.", isNumPatients);
 	}
 
-	//AVTQuery - query AD database - basic flow. 
+	//AVTQuery - query AD database - basic flow (Study level query test). 
 	//Parameters: adCriteria, adAimCriteria, ADQueryTarget, previousSearchResult, queryObject are all correct.
-	public void testQueryAD_1B(){
-		AttributeList attList = setup.getCriteria();					
-		Map<Integer, Object> adCriteria1 = DicomUtil.convertToADDicomCriteria(attList);
-		Map<String, Object> adAimCriteria1 = new HashMap<String, Object>();
-		AVTQuery avtQuery1 = new AVTQuery(adCriteria1, adAimCriteria1, QueryTarget.PATIENT, null, null);
-		avtQuery1.addDataAccessListener(this);
-		Thread t1 = new Thread(avtQuery1);
+	@Test
+	public void testQueryAD_1B(){					
+		Map<Integer, Object> adDicomCriteria = DicomUtil.convertToADDicomCriteria(attList);
+		Map<String, Object> adAimCriteria = new HashMap<String, Object>();
+		Query avtQuery = new AVTQuery(adDicomCriteria, adAimCriteria, QueryTarget.PATIENT, null, null);
+		avtQuery.addDataAccessListener(this);
+		Thread t1 = new Thread(avtQuery);
 		t1.start();	
 		try {
 			t1.join();
 		} catch (InterruptedException e) {			
-			e.printStackTrace();
+			logger.error(e, e);
 		}
 		Patient selectedNode = result.getPatients().get(0);
 		
-		Map<String, Object> adAimCriteria = new HashMap<String, Object>();		
+		Map<String, Object> adAimCriteria2 = new HashMap<String, Object>();		
 		String key = "ImagingObservationCharacteristic.codeMeaning";
 		Object value = "irregularly shaped";
-		adAimCriteria.put(key, value);
-							
-		Map<Integer, Object> adCriteria = DicomUtil.convertToADDicomCriteria(attList);
+		adAimCriteria2.put(key, value);
 		
-		AVTQuery avtQuery = new AVTQuery(adCriteria, adAimCriteria, QueryTarget.STUDY, result, selectedNode);
-		avtQuery.addDataAccessListener(this);
-		Thread t2 = new Thread(avtQuery);
+		Query avtQuery2 = new AVTQuery(adDicomCriteria, adAimCriteria2, QueryTarget.STUDY, result, selectedNode);
+		avtQuery2.addDataAccessListener(this);
+		Thread t2 = new Thread(avtQuery2);
 		t2.start();	
 		try {
 			t2.join();
 		} catch (InterruptedException e) {			
-			e.printStackTrace();
+			logger.error(e, e);
 		}
 		String patientID = "YAMAMOTO-00046";
 		boolean isQueryOK = result.contains(patientID);
 		boolean isNumPatients = (result.getPatients().size() == 1);
 		int numStudies = result.getPatients().get(0).getStudies().size();
 		boolean isNumStudies = (numStudies == 1);	
-		assertTrue("The number of patients in AD > 0 but system unable to find data.", isQueryOK);
+		assertTrue("Unable to find specified patientID..", isQueryOK);
 		assertTrue("Actual number of found patients is different than 1.", isNumPatients);
 		assertTrue("Actual number of found studies is " + numStudies + ". Expected 1.", isNumStudies);
 	}
 	
+	//AVT AD query test directly with adServive 
+	@Test
 	public void testQueryAD_1C(){
-		//AttributeList attList = setup.getCriteria();
-		//Map<Integer, Object> adDicomCriteria = DicomUtil.convertToADDicomCriteria(attList);		
 		Map<Integer, Object> adDicomCriteria = new HashMap<Integer, Object>();
 		adDicomCriteria.put(new Integer(1048608), "YAMAMOTO-00046");
-		//Map<Integer, Object> adDicomCriteria = new HashMap<Integer, Object>();
 		Set<Integer> keySet = adDicomCriteria.keySet();
 		Iterator<Integer> iter = keySet.iterator();
-		System.out.println("-----------------------------------------------------------------------------");
+		logger.debug("AD DICOM criteria:");
 		while(iter.hasNext()){
 			Integer key = iter.next();
 			String value = (String) adDicomCriteria.get(key);			
-			System.out.println(key + " " + value);
-		}
-		System.out.println("-----------------------------------------------------------------------------");	
+			logger.debug(key + " " + value);
+		}	
 		Map<String, Object> adAimCriteria = new HashMap<String, Object>();		
 		String key = "ImagingObservationCharacteristic.codeMeaning";
 		Object value = "irregularly shaped";
@@ -147,31 +137,10 @@ public class QueryADTest extends TestCase implements DataAccessListener{
 		List<com.siemens.scr.avt.ad.dicom.Patient> patients = adService.findPatientByCriteria(adDicomCriteria, adAimCriteria);
 		assertTrue("Expected number of patients: 1. " + "Actual: " + patients.size(), patients.size() == 1);
 		List<GeneralStudy> studies = adService.findStudiesByCriteria(adDicomCriteria, adAimCriteria);
-		assertTrue("Expected number of studies: 1. " + "Actual: " + studies.size(), studies.size() == 1);					
-			
+		assertTrue("Expected number of studies: 1. " + "Actual: " + studies.size(), studies.size() == 1);						
 	}
 	
 	
-	//AVTQuery - query AD database - alternative flow. 
-	//Parameters: adCriteria, ADQueryTarget, previousSearchResult, queryObject are all correct.
-	//Multi-query requests sent all in parallel (two requests)
-	/*
-	public void testQueryAD_1B(){
-		AttributeList attList = setup.getCriteria();					
-		Map<Integer, Object> adCriteria = DicomUtil.convertToADDicomCriteria(attList);
-		Map<String, Object> adAimCriteria = new HashMap<String, Object>();
-		AVTQuery avtQuery = new AVTQuery(adCriteria, adAimCriteria, ADQueryTarget.PATIENT, null, null);
-		avtQuery.addAVTListener(this);
-		Thread t = new Thread(avtQuery);
-		t.start();	
-		try {
-			t.join();
-		} catch (InterruptedException e) {			
-			e.printStackTrace();
-		}
-		
-	}
-	*/
 	@Override
 	public void retrieveResultsAvailable(RetrieveEvent e) {
 		// TODO Auto-generated method stub
@@ -181,7 +150,8 @@ public class QueryADTest extends TestCase implements DataAccessListener{
 	SearchResult result;
 	@Override
 	public void queryResultsAvailable(QueryEvent e) {
-		result = (SearchResult) e.getSource();			
+		Query query = (Query) e.getSource();
+		result = query.getSearchResult();		
 	}
 
 	@Override
