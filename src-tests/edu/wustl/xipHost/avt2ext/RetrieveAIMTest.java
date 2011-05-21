0b1db8501d88957757345e4b3f94c970447efac2
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-05-21 11:30:42 +0000

    Modified RetrieveAIMTest and RetrieveDicomSegTest JUnit tests.
    SVN-Revision: 932

--- src-tests/edu/wustl/xipHost/avt2ext/RetrieveAIMTest.java
+++ src-tests/edu/wustl/xipHost/avt2ext/RetrieveAIMTest.java
@@ -3,53 +3,80 @@
  */
 package edu.wustl.xipHost.avt2ext;
 
+import static org.junit.Assert.assertEquals;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.dcm4che2.data.Tag;
-
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import com.siemens.scr.avt.ad.annotation.ImageAnnotation;
 import com.siemens.scr.avt.ad.api.ADFacade;
-
 import edu.wustl.xipHost.avt2ext.AVTFactory;
-
-import junit.framework.TestCase;
 
 /**
  * @author Jaroslaw Krych
  *
  */
-public class RetrieveAIMTest extends TestCase {
-	ADFacade adService;	
-	/**
-	 * @param name
-	 */
-	public RetrieveAIMTest(String name) {
-		super(name);
-	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
+public class RetrieveAIMTest {
+	final static Logger logger = Logger.getLogger(RetrieveAIMTest.class);
+	static ADFacade adService;	
+	static SAXBuilder builder;
+	Document document;
+	static XMLOutputter outToXMLFile;
+	static JAXBContext jc;
+	static Unmarshaller u = null;
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
 		adService = AVTFactory.getADServiceInstance();
+		builder = new SAXBuilder();
+		outToXMLFile = new XMLOutputter();
+		try {
+			jc = JAXBContext.newInstance( "gme.cacore_cacore._3_2.edu_northwestern_radiology" );
+			u = jc.createUnmarshaller();
+		} catch (JAXBException e) {			
+			logger.error(e, e);
+		}
+		DOMConfigurator.configure("log4j.xml");
 	}
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	@AfterClass
+	public static void tearDown() throws Exception {
+		
 	}
 	
+	@Test
 	public void testRetrieveAIM_1A(){
 		Map<Integer, Object> dicomCriteria = new HashMap<Integer, Object>();
 		dicomCriteria.put(Tag.SeriesInstanceUID, "1.2.840.113704.1.111.4280.1226686843.113");
 		List<ImageAnnotation> imageAnnots = adService.retrieveAnnotations(dicomCriteria, null);
 		ImageAnnotation annot = (ImageAnnotation)imageAnnots.get(0);
-		System.out.println(annot.getAIM());
+		//Assert image annotation uniqueIdentifier
+		String strXML = annot.getAIM();
+		InputStreamReader isr = new InputStreamReader(IOUtils.toInputStream(strXML));		
+		try {
+			JAXBElement<?> obj = (JAXBElement<?>)u.unmarshal(isr);
+			gme.cacore_cacore._3_2.edu_northwestern_radiology.ImageAnnotation imageAnnotation = ((gme.cacore_cacore._3_2.edu_northwestern_radiology.ImageAnnotation)obj.getValue());
+			String annotID = String.valueOf(imageAnnotation.getUniqueIdentifier());
+			//System.out.println(annot.getAIM());
+			assertEquals("Wrong annotation retrieved. Expected ID: " + "1.3.6.1.4.1.5962.99.1.1772356583.1829344988.1264492774375.3.0" +
+					" but actual: " + annotID, annotID, "1.3.6.1.4.1.5962.99.1.1772356583.1829344988.1264492774375.3.0");
+		} catch (JAXBException e) {
+			logger.error(e, e);
+		}							        
 	}
 
 }
--- src-tests/edu/wustl/xipHost/avt2ext/RetrieveDicomSegTest.java
+++ src-tests/edu/wustl/xipHost/avt2ext/RetrieveDicomSegTest.java
@@ -3,6 +3,7 @@
  */
 package edu.wustl.xipHost.avt2ext;
 
+import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,53 +11,60 @@ import java.util.List;
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
 import org.dcm4che2.io.DicomOutputStream;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import com.siemens.scr.avt.ad.api.ADFacade;
 import edu.wustl.xipHost.avt2ext.AVTFactory;
-import junit.framework.TestCase;
 
 /**
  * @author Jaroslaw Krych
  *
  */
-public class RetrieveDicomSegTest extends TestCase {
-	ADFacade adService;	
-	/**
-	 * @param name
-	 */
-	public RetrieveDicomSegTest(String name) {
-		super(name);
-	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
+public class RetrieveDicomSegTest {
+	static ADFacade adService;	
+	
+	@BeforeClass
+	public static void setUp()throws Exception {
 		adService = AVTFactory.getADServiceInstance();
 	}
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	@AfterClass
+	public static void tearDown() throws Exception {
+		
 	}
 	
+	@Test
 	public void testRetrieveDicomSegTest_1A() throws IOException{
-		String aimUID1 = "1.3.6.1.4.1.5962.99.1.1772131551.1017304176.1264492549343.1.0";
 		String aimUID2 = "1.3.6.1.4.1.5962.99.1.1772356583.1829344988.1264492774375.3.0";
 		List<DicomObject> dicomSegObjs = adService.retrieveSegmentationObjects(aimUID2);
-		for(int i = 0; i < dicomSegObjs.size(); i++){
-    		DicomObject dicom = dicomSegObjs.get(i);
+		int numSegObjects;
+		boolean dicomSEG1 = false;
+		boolean dicomSEG2 = false;
+		for(numSegObjects = 0; numSegObjects < dicomSegObjs.size(); numSegObjects++){
+    		DicomObject dicom = dicomSegObjs.get(numSegObjects);
     		String sopInstanceUID = dicom.getString(Tag.SOPInstanceUID);
     		DicomObject segDicom = adService.getDicomObject(sopInstanceUID);
+    		String dicomSEGSOPInstanceUID = segDicom.getString(Tag.SOPInstanceUID);
 			String dirPath = new File("./test-content/AVT2EXT_SEG_Retrieve").getCanonicalPath() + File.separator;
     		String fileName = dirPath + sopInstanceUID + ".dcm";
 			File file = new File(fileName);
 			DicomOutputStream dout = new DicomOutputStream(new FileOutputStream(fileName));
 			dout.writeDicomFile(segDicom);
 			dout.close();
-			System.out.println(file.length() / 1024);
-    	}	
+			long fileLength = (file.length() / 1024);		
+			
+			if(dicomSEGSOPInstanceUID.equalsIgnoreCase("1.2.276.0.7230010.3.1.4.2554264370.29928.1264492790.3")){
+				dicomSEG1 = true;
+			} else if(dicomSEGSOPInstanceUID.equalsIgnoreCase("1.2.276.0.7230010.3.1.4.2554264370.29928.1264492801.5")){
+				dicomSEG2 = true;
+			}			
+			//Assert retrieved file size
+			assertTrue("", fileLength > 0);
+    	}
+		//Assert number of retrieved DICOM SEG objects
+		assertEquals("Number of retrieved DICOM SEG object is not as expected.", 2, numSegObjects);
+		//Assert DICOM SEG object SOPInstanceUID
+		assertTrue("Wrong retrieved DICOM SEG objects.", dicomSEG1 == true && dicomSEG2 == true);
 	}
 }
