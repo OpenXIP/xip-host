d1ae4fa5adb5730d8b4aa68c74c2c50071a06974
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-11-05 22:44:45 +0000

    Added all retrieve targets to AVTRetrieve (DICOM, AIM plus SEG and DICOM plus AIM plus SEG). Externalized retrieve logic into retrieveDICOM() and  retrieveAIMandDICOMSeg() methods.
    SVN-Revision: 985

--- src/edu/wustl/xipHost/avt2ext/AVTRetrieve.java
+++ src/edu/wustl/xipHost/avt2ext/AVTRetrieve.java
@@ -130,6 +130,28 @@ public class AVTRetrieve implements Retrieve {
 	Document document;
 	XMLOutputter outToXMLFile = new XMLOutputter();
 	Map<String, ObjectLocator> retrieve(Map<Integer, Object> dicomCriteria, Map<String, Object> aimCriteria, File importDir, RetrieveTarget retrieveTarget) throws IOException {		
+		Map<String, ObjectLocator> objectLocators = new HashMap<String, ObjectLocator>();				
+		if(retrieveTarget == RetrieveTarget.DICOM_AND_AIM){
+			objectLocators = retrieveDICOM();
+			Map<String, ObjectLocator> objectLocatorsAIMandSEG = retrieveAIMandDICOMSeg();
+			Iterator<String> keySet = objectLocatorsAIMandSEG.keySet().iterator();
+			while(keySet.hasNext()){
+				String key = keySet.next();
+				ObjectLocator objLoc = objectLocatorsAIMandSEG.get(key);
+				objectLocators.put(key, objLoc);
+			}
+		} else if (retrieveTarget == RetrieveTarget.DICOM) {
+			objectLocators = retrieveDICOM();
+		} else if (retrieveTarget == RetrieveTarget.AIM_SEG) {
+			objectLocators = retrieveAIMandDICOMSeg();
+		}
+		return objectLocators;
+	}	
+	
+	Map<String, ObjectLocator> retrieveDICOM(){
+		//Retrieve DICOM						
+		//If oneSeries contains subset of items, narrow dicomCriteria to individual SOPInstanceUIDs
+		//Then retrieve data item by item	
 		logger.debug("DICOM retrieve criteria: ");
 		Iterator<Integer> keySet = dicomCriteria.keySet().iterator();
 		while(keySet.hasNext()){
@@ -137,179 +159,109 @@ public class AVTRetrieve implements Retrieve {
 			Object value = dicomCriteria.get(key);
 			logger.debug("Tag: " + DicomUtil.toDicomHex(key) + " Value: " + value.toString());
 		}
-		
-		Map<String, ObjectLocator> objectLocators = new HashMap<String, ObjectLocator>();		
-		File dirPath = importDir.getAbsoluteFile();				
-		if(retrieveTarget == RetrieveTarget.DICOM_AND_AIM){
-			//Retrieve DICOM						
-			//If oneSeries contains subset of items, narrow dicomCriteria to individual SOPInstanceUIDs
-			//Then retrieve data item by item			
-			List<DicomObject> retrievedDICOM = adService.retrieveDicomObjs(dicomCriteria, aimCriteria);											
-			int i;
-			for(i = 0; i < retrievedDICOM.size(); i++){
-				DicomObject dicom = retrievedDICOM.get(i);							
-				String filePrefix = dicom.getString(Tag.SOPInstanceUID);
-				try {
-					File file = new File(importDir.getAbsolutePath() + File.separatorChar + filePrefix);
-					if(!file.exists()){
-						file.createNewFile();
-					}
-					FileOutputStream fos = new FileOutputStream(file);
-					BufferedOutputStream bos = new BufferedOutputStream(fos);
-					DicomOutputStream dout = new DicomOutputStream(bos);
-					dout.writeDicomFile(dicom);
-					dout.close();
-					ObjectLocator objLoc = new ObjectLocator();				
-					//ObjectDescriptor objDesc = objectDescriptors.get(i);
-					ObjectDescriptor objDesc = objDescsDICOM.get(i);
-					Uuid itemUUID = objDesc.getUuid();
-					objLoc.setUuid(itemUUID);				
-					objLoc.setUri(file.getAbsolutePath()); 
-					objectLocators.put(itemUUID.getUuid(), objLoc);						
-				} catch (IOException e) {
-					logger.error(e, e);
-					return null;
-				}									
-			}			
-			//Retrieve AIM		
-			List<String> annotationUIDs = adService.findAnnotations(dicomCriteria, aimCriteria);
-			Set<String> uniqueAnnotUIDs = new HashSet<String>(annotationUIDs);
-			Iterator<String> iter = uniqueAnnotUIDs.iterator();
-			Iterator<ObjectDescriptor> iterObjDescsAIM = objDescsAIM.iterator();
-			while(iter.hasNext()){
-				String uid = iter.next();
-				ImageAnnotation loadedAnnot = adService.getAnnotation(uid);
-				String strXML = loadedAnnot.getAIM();
-				byte[] source = strXML.getBytes();
-				InputStream is = new ByteArrayInputStream(source);
-				try {
-					document = builder.build(is);
-				} catch (JDOMException e) {
-					logger.error(e, e);
-					return null;
-				}	
-				File outFile = new File(dirPath + File.separator + uid);
-				FileOutputStream outStream = new FileOutputStream(outFile);			
-				outToXMLFile.output(document, outStream);
-		    	outStream.flush();
-		    	outStream.close();
-		    	//retrievedFiles.add(outFile);
-		    	ObjectLocator objLoc = new ObjectLocator();	
-		    	//ObjectDescriptor objDesc = objectDescriptors.get(i);
-		    	ObjectDescriptor objDesc = iterObjDescsAIM.next();			    	
-	    		Uuid itemUUID = objDesc.getUuid();			    	
+		Map<String, ObjectLocator> objectLocators = new HashMap<String, ObjectLocator>();
+		List<DicomObject> retrievedDICOM = adService.retrieveDicomObjs(dicomCriteria, aimCriteria);											
+		int i;
+		for(i = 0; i < retrievedDICOM.size(); i++){
+			DicomObject dicom = retrievedDICOM.get(i);							
+			String filePrefix = dicom.getString(Tag.SOPInstanceUID);
+			try {
+				File file = new File(importDir.getAbsolutePath() + File.separatorChar + filePrefix);
+				if(!file.exists()){
+					file.createNewFile();
+				}
+				FileOutputStream fos = new FileOutputStream(file);
+				BufferedOutputStream bos = new BufferedOutputStream(fos);
+				DicomOutputStream dout = new DicomOutputStream(bos);
+				dout.writeDicomFile(dicom);
+				dout.close();
+				ObjectLocator objLoc = new ObjectLocator();				
+				//ObjectDescriptor objDesc = objectDescriptors.get(i);
+				ObjectDescriptor objDesc = objDescsDICOM.get(i);
+				Uuid itemUUID = objDesc.getUuid();
 				objLoc.setUuid(itemUUID);				
-				objLoc.setUri(outFile.getAbsolutePath()); 
-				objectLocators.put(itemUUID.getUuid(), objLoc);
-		    	
-				//Retrieve DICOM SEG objects
-		    	Set<String> dicomSegSOPInstanceUIDs = new HashSet<String>();
-		    	List<DicomObject> segObjects = adService.retrieveSegmentationObjects(uid);
-		    	for(int j = 0; j < segObjects.size(); j++){
-		    		DicomObject dicom = segObjects.get(j);
-		    		String sopInstanceUID = dicom.getString(Tag.SOPInstanceUID);
-		    		//Check if DICOM SEG was not serialized in reference to another AIM
-		    		if(!dicomSegSOPInstanceUIDs.contains(sopInstanceUID)){
-		    			dicomSegSOPInstanceUIDs.add(sopInstanceUID);
-		    			DicomObject dicomSeg = adService.getDicomObject(sopInstanceUID);
-		    			String message = "DICOM SEG " + sopInstanceUID + " cannot be loaded from file system!";
-		    			if(dicomSeg == null){			    				
-		    				throw new FileNotFoundException(message);
-		    			} else {					 
-		    				//TODO DICOM SEG tmp file not found e.g. DICOM SEG belongs to not specified Study for which TargetIteratorRunner was not requested					 					    			
-	    					File outDicomSegFile = new File(dirPath + File.separator + sopInstanceUID);
-    						FileOutputStream fos = new FileOutputStream(outDicomSegFile);
-    						BufferedOutputStream bos = new BufferedOutputStream(fos);
-    						DicomOutputStream dout = new DicomOutputStream(bos);
-    						dout.writeDicomFile(dicomSeg);
-    						dout.close();
-    						ObjectLocator dicomSegObjLoc = new ObjectLocator();
-    						Uuid dicomSegItemUUID = new Uuid();
-    						dicomSegItemUUID.setUuid(UUID.randomUUID().toString());
-    				    	ObjectDescriptor objDescDicomSeg = new ObjectDescriptor();
-    				    	objDescDicomSeg.setUuid(dicomSegItemUUID);
-    				    	//TODO set MIME type, sopClass UID etc.
-    				    	objectDescriptors.add(objDescDicomSeg); 						
-							dicomSegObjLoc.setUuid(dicomSegItemUUID);				
-							dicomSegObjLoc.setUri(outDicomSegFile.getAbsolutePath()); 
-							objectLocators.put(dicomSegItemUUID.getUuid(), dicomSegObjLoc);
-		    			}
-		    		}
-		    	}	  
-			}
-		} else if (retrieveTarget == RetrieveTarget.DICOM) {
-			
-		} else if (retrieveTarget == RetrieveTarget.AIM_SEG) {
-			logger.debug("AVT AD RetrieveTarget = " + retrieveTarget.toString());
-			//Retrieve AIM		
-			List<String> annotationUIDs = adService.findAnnotations(dicomCriteria, aimCriteria);
-			Set<String> uniqueAnnotUIDs = new HashSet<String>(annotationUIDs);
-			Iterator<String> iter = uniqueAnnotUIDs.iterator();
-			Iterator<ObjectDescriptor> iterObjDescsAIM = objDescsAIM.iterator();
-			while(iter.hasNext()){
-				String uid = iter.next();
-				ImageAnnotation loadedAnnot = adService.getAnnotation(uid);
-				String strXML = loadedAnnot.getAIM();
-				byte[] source = strXML.getBytes();
-				InputStream is = new ByteArrayInputStream(source);
-				try {
-					document = builder.build(is);
-				} catch (JDOMException e) {
-					logger.error(e, e);
-					return null;
-				}	
-				File outFile = new File(dirPath + File.separator + uid);
-				FileOutputStream outStream = new FileOutputStream(outFile);			
-				outToXMLFile.output(document, outStream);
-		    	outStream.flush();
-		    	outStream.close();
-		    	//retrievedFiles.add(outFile);
-		    	ObjectLocator objLoc = new ObjectLocator();	
-		    	//ObjectDescriptor objDesc = objectDescriptors.get(i);
-		    	ObjectDescriptor objDesc = iterObjDescsAIM.next();			    	
-	    		Uuid itemUUID = objDesc.getUuid();			    	
-				objLoc.setUuid(itemUUID);				
-				objLoc.setUri(outFile.getAbsolutePath()); 
-				objectLocators.put(itemUUID.getUuid(), objLoc);
-		    	
-				//Retrieve DICOM SEG objects
-		    	Set<String> dicomSegSOPInstanceUIDs = new HashSet<String>();
-		    	List<DicomObject> segObjects = adService.retrieveSegmentationObjects(uid);
-		    	for(int j = 0; j < segObjects.size(); j++){
-		    		DicomObject dicom = segObjects.get(j);
-		    		String sopInstanceUID = dicom.getString(Tag.SOPInstanceUID);
-		    		//Check if DICOM SEG was not serialized in reference to another AIM
-		    		if(!dicomSegSOPInstanceUIDs.contains(sopInstanceUID)){
-		    			dicomSegSOPInstanceUIDs.add(sopInstanceUID);
-		    			DicomObject dicomSeg = adService.getDicomObject(sopInstanceUID);
-		    			String message = "DICOM SEG " + sopInstanceUID + " cannot be loaded from file system!";
-		    			if(dicomSeg == null){			    				
-		    				throw new FileNotFoundException(message);
-		    			} else {					 
-		    				//TODO DICOM SEG tmp file not found e.g. DICOM SEG belongs to not specified Study for which TargetIteratorRunner was not requested					 					    			
-	    					File outDicomSegFile = new File(dirPath + File.separator + sopInstanceUID);
-    						FileOutputStream fos = new FileOutputStream(outDicomSegFile);
-    						BufferedOutputStream bos = new BufferedOutputStream(fos);
-    						DicomOutputStream dout = new DicomOutputStream(bos);
-    						dout.writeDicomFile(dicomSeg);
-    						dout.close();
-    						ObjectLocator dicomSegObjLoc = new ObjectLocator();
-    						Uuid dicomSegItemUUID = new Uuid();
-    						dicomSegItemUUID.setUuid(UUID.randomUUID().toString());
-    				    	ObjectDescriptor objDescDicomSeg = new ObjectDescriptor();
-    				    	objDescDicomSeg.setUuid(dicomSegItemUUID);
-    				    	//TODO set MIME type, sopClass UID etc.
-    				    	objectDescriptors.add(objDescDicomSeg); 						
-							dicomSegObjLoc.setUuid(dicomSegItemUUID);				
-							dicomSegObjLoc.setUri(outDicomSegFile.getAbsolutePath()); 
-							objectLocators.put(dicomSegItemUUID.getUuid(), dicomSegObjLoc);
-		    			}
-		    		}
-		    	}	  
-			}
+				objLoc.setUri(file.getAbsolutePath()); 
+				objectLocators.put(itemUUID.getUuid(), objLoc);						
+			} catch (IOException e) {
+				logger.error(e, e);
+				return null;
+			}									
 		}
 		return objectLocators;
-	}		
+	}
+	
+	Map<String, ObjectLocator> retrieveAIMandDICOMSeg() throws IOException {
+		//Retrieve AIM	and DICOM SEG objects	
+		logger.debug("AVT AD RetrieveTarget = " + retrieveTarget.toString());
+		Map<String, ObjectLocator> objectLocators = new HashMap<String, ObjectLocator>();
+		File dirPath = importDir.getAbsoluteFile();				
+		List<String> annotationUIDs = adService.findAnnotations(dicomCriteria, aimCriteria);
+		Set<String> uniqueAnnotUIDs = new HashSet<String>(annotationUIDs);
+		Iterator<String> iter = uniqueAnnotUIDs.iterator();
+		Iterator<ObjectDescriptor> iterObjDescsAIM = objDescsAIM.iterator();
+		while(iter.hasNext()){
+			String uid = iter.next();
+			ImageAnnotation loadedAnnot = adService.getAnnotation(uid);
+			String strXML = loadedAnnot.getAIM();
+			byte[] source = strXML.getBytes();
+			InputStream is = new ByteArrayInputStream(source);
+			try {
+				document = builder.build(is);
+			} catch (JDOMException e) {
+				logger.error(e, e);
+				return null;
+			}	
+			File outFile = new File(dirPath + File.separator + uid);
+			FileOutputStream outStream = new FileOutputStream(outFile);			
+			outToXMLFile.output(document, outStream);
+	    	outStream.flush();
+	    	outStream.close();
+	    	//retrievedFiles.add(outFile);
+	    	ObjectLocator objLoc = new ObjectLocator();	
+	    	//ObjectDescriptor objDesc = objectDescriptors.get(i);
+	    	ObjectDescriptor objDesc = iterObjDescsAIM.next();			    	
+    		Uuid itemUUID = objDesc.getUuid();			    	
+			objLoc.setUuid(itemUUID);				
+			objLoc.setUri(outFile.getAbsolutePath()); 
+			objectLocators.put(itemUUID.getUuid(), objLoc);
+	    	
+			//Retrieve DICOM SEG objects
+	    	Set<String> dicomSegSOPInstanceUIDs = new HashSet<String>();
+	    	List<DicomObject> segObjects = adService.retrieveSegmentationObjects(uid);
+	    	for(int j = 0; j < segObjects.size(); j++){
+	    		DicomObject dicom = segObjects.get(j);
+	    		String sopInstanceUID = dicom.getString(Tag.SOPInstanceUID);
+	    		//Check if DICOM SEG was not serialized in reference to another AIM
+	    		if(!dicomSegSOPInstanceUIDs.contains(sopInstanceUID)){
+	    			dicomSegSOPInstanceUIDs.add(sopInstanceUID);
+	    			DicomObject dicomSeg = adService.getDicomObject(sopInstanceUID);
+	    			String message = "DICOM SEG " + sopInstanceUID + " cannot be loaded from file system!";
+	    			if(dicomSeg == null){			    				
+	    				throw new FileNotFoundException(message);
+	    			} else {					 
+	    				//TODO DICOM SEG tmp file not found e.g. DICOM SEG belongs to not specified Study for which TargetIteratorRunner was not requested					 					    			
+    					File outDicomSegFile = new File(dirPath + File.separator + sopInstanceUID);
+						FileOutputStream fos = new FileOutputStream(outDicomSegFile);
+						BufferedOutputStream bos = new BufferedOutputStream(fos);
+						DicomOutputStream dout = new DicomOutputStream(bos);
+						dout.writeDicomFile(dicomSeg);
+						dout.close();
+						ObjectLocator dicomSegObjLoc = new ObjectLocator();
+						Uuid dicomSegItemUUID = new Uuid();
+						dicomSegItemUUID.setUuid(UUID.randomUUID().toString());
+				    	ObjectDescriptor objDescDicomSeg = new ObjectDescriptor();
+				    	objDescDicomSeg.setUuid(dicomSegItemUUID);
+				    	//TODO set MIME type, sopClass UID etc.
+				    	objectDescriptors.add(objDescDicomSeg); 						
+						dicomSegObjLoc.setUuid(dicomSegItemUUID);				
+						dicomSegObjLoc.setUri(outDicomSegFile.getAbsolutePath()); 
+						objectLocators.put(dicomSegItemUUID.getUuid(), dicomSegObjLoc);
+	    			}
+	    		}
+	    	}	  
+		}
+		return objectLocators;
+	}
 	
 	@Override
 	public void addDataAccessListener(DataAccessListener l) {
