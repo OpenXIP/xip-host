bee8eafd8711d35e86eee147c2a73e21ef5e6bdc
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-11-05 22:58:57 +0000

    Modified InsertDICOM in dicom package to make it Mac OS X compatible.
    SVN-Revision: 992

--- src/edu/wustl/xipHost/dicom/InsertDICOM.java
+++ src/edu/wustl/xipHost/dicom/InsertDICOM.java
@@ -4,6 +4,8 @@
 package edu.wustl.xipHost.dicom;
 
 import java.io.File;
+import java.io.FilenameFilter;
+
 import edu.wustl.xipHost.localFileSystem.HostFileChooser;
 
 /**
@@ -15,15 +17,22 @@ public class InsertDICOM {
 	public static void main(String[] args) {
 		HostFileChooser fileChooser = new HostFileChooser(true, new File("./dicom-dataset-demo"));
 		fileChooser.setVisible(true);
-		File[] files = fileChooser.getSelectedItems();
+		//File[] files = fileChooser.getSelectedItems();
+		File[] files = new File("./dicom-dataset-demo").listFiles(filter);
 		if(files == null){
 			return;
 		}		
-		//PacsLocation loc = new PacsLocation("127.0.0.1", 3001, "WORKSTATION1", "XIPHost embedded database");
-		PacsLocation loc = new PacsLocation("127.0.0.1", 3002, "WORKSTATION2", "XIPHost test database");
+		PacsLocation loc = new PacsLocation("127.0.0.1", 3001, "WORKSTATION1", "XIPHost embedded database");
+		//PacsLocation loc = new PacsLocation("127.0.0.1", 3002, "WORKSTATION2", "XIPHost test database");
 		DicomManager dicomMgr = DicomManagerFactory.getInstance();
 		dicomMgr.runDicomStartupSequence();
 		DicomManagerFactory.getInstance().submit(files, loc);		
 		System.exit(0);
 	}
+	
+	static FilenameFilter filter = new FilenameFilter() {
+	    public boolean accept(File dir, String name) {
+	        return !name.startsWith(".svn");
+	    }
+	};
 }
