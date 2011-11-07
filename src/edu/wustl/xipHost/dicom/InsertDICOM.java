0a655f6ca6b5c887bf02304ef43412ea885f3d3d
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-11-07 22:40:19 +0000

    Modified InsertDICOM to make it Mac OS X compatible.
    SVN-Revision: 1000

--- src/edu/wustl/xipHost/dicom/InsertDICOM.java
+++ src/edu/wustl/xipHost/dicom/InsertDICOM.java
@@ -4,8 +4,7 @@
 package edu.wustl.xipHost.dicom;
 
 import java.io.File;
-import java.io.FilenameFilter;
-
+import javax.swing.JFrame;
 import edu.wustl.xipHost.localFileSystem.HostFileChooser;
 
 /**
@@ -16,9 +15,8 @@ public class InsertDICOM {
 
 	public static void main(String[] args) {
 		HostFileChooser fileChooser = new HostFileChooser(true, new File("./dicom-dataset-demo"));
-		fileChooser.setVisible(true);
-		//File[] files = fileChooser.getSelectedItems();
-		File[] files = new File("./dicom-dataset-demo").listFiles(filter);
+		fileChooser.showOpenDialog(new JFrame());	
+		File[] files = fileChooser.getSelectedFiles();
 		if(files == null){
 			return;
 		}		
@@ -29,10 +27,4 @@ public class InsertDICOM {
 		DicomManagerFactory.getInstance().submit(files, loc);		
 		System.exit(0);
 	}
-	
-	static FilenameFilter filter = new FilenameFilter() {
-	    public boolean accept(File dir, String name) {
-	        return !name.startsWith(".svn");
-	    }
-	};
 }
