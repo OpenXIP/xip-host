79639445ff66966cfb22b9be287f08dc821baa80
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-12-07 16:28:49 +0000

    Updated UpdateSelectedDataSearchResultProgressiveTest JUnit test.
    SVN-Revision: 1044

--- src-tests/edu/wustl/xipHost/gui/checkboxTree/UpdateSelectedDataSearchResultProgressiveTest.java
+++ src-tests/edu/wustl/xipHost/gui/checkboxTree/UpdateSelectedDataSearchResultProgressiveTest.java
@@ -13,9 +13,8 @@ import java.util.Map;
 import javax.swing.tree.DefaultMutableTreeNode;
 import org.apache.log4j.Logger;
 import org.dcm4che2.data.Tag;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import edu.wustl.xipHost.dataModel.Item;
 import edu.wustl.xipHost.dataModel.Patient;
@@ -39,25 +38,31 @@ public class UpdateSelectedDataSearchResultProgressiveTest implements DataSelect
 	/**
 	 * @throws java.lang.Exception
 	 */
-	public UpdateSelectedDataSearchResultProgressiveTest(){
+	/*public UpdateSelectedDataSearchResultProgressiveTest(){
 		resultTree = new SearchResultTreeProgressive();
 		selectedDataSearchResult = new SearchResult();
 		resultTree.setSelectedDataSearchResult(selectedDataSearchResult);
-	}
+	}*/
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUpBeforeTest() throws Exception {
 		result = new SearchResult("Test Progressive");
 		dicomCriteria.put(Tag.PatientName, "*");
 		Criteria criteria = new Criteria(dicomCriteria, aimCriteria);
 		result.setOriginalCriteria(criteria);
+		resultTree = new SearchResultTreeProgressive();
+		selectedDataSearchResult = new SearchResult();
+		resultTree.setSelectedDataSearchResult(selectedDataSearchResult);
 	}
 
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@After
+	public void tearDownAfterTest() throws Exception {
+		result = null;
+		resultTree = null;
+		selectedDataSearchResult = null;
 	}
 	
 	//SearchResultTree 1A - basic flow. One PatientNode selected. Patient not sub-queried.
@@ -94,8 +99,7 @@ public class UpdateSelectedDataSearchResultProgressiveTest implements DataSelect
 	}
 
 	//SearchResultTree 2A - basic flow. One PatientNode selected. No Studies found for Patient. Then selected Patient sub-queried.
-	//Result: selectedDataSearchResult should contain one patient, and all studied found in selected Patient.
-	@Ignore("Test suspends the system due to the synchronization problem")
+	//Result: selectedDataSearchResult should contain one patient, and all studies found in selected Patient.
 	@Test
 	public void testUpdatedSelectedDataSearchResult2A() {
 		resultTree.addDataSelectionListener(this);
@@ -123,6 +127,8 @@ public class UpdateSelectedDataSearchResultProgressiveTest implements DataSelect
 		selectedPatient.addStudy(study1);
 		selectedPatient.addStudy(study2);
 		resultTree.updateNodes(result);
+		resultTree.setDoubleClicked(true);
+		resultTree.updateNodeProgressive(patientNode1);
 		synchronized(this){
 			while(selectedDataSearchResult.getPatients().get(0).getStudies().size() == 0){
 				try {
@@ -143,6 +149,113 @@ public class UpdateSelectedDataSearchResultProgressiveTest implements DataSelect
 		int numbOfStudies = studies.size();
 		assertEquals("Invalid number of studies. Expected: 2, actual: " + numbOfStudies, 2, numbOfStudies);
 	}
+	
+	//SearchResultTree 3A - basic flow. One PatientNode and two Studies found for Patient no initially selected. Then Study is selected and sub-queried.
+	//Result: selectedDataSearchResult should contain one Patient, one Study and all Series found in selected Study.
+	@Test
+	public void testUpdatedSelectedDataSearchResult3A() {
+		resultTree.addDataSelectionListener(this);
+		Patient patient1 = new Patient("Jarek1", "111", "07/18/1973");
+		result.addPatient(patient1);
+		resultTree.updateNodes(result);
+		DefaultMutableTreeNode rootNode = resultTree.getRootNode();
+		DefaultMutableTreeNode locationNode = (DefaultMutableTreeNode)rootNode.getChildAt(0);
+		PatientNode patientNode1 = (PatientNode)locationNode.getChildAt(0);
+		resultTree.updateSelection(patientNode1, true);
+		synchronized(this){
+			while(selectedDataSearchResult.getPatients().size() == 0){
+				try {
+					this.wait();
+				} catch (InterruptedException e1) {
+					logger.error(e1, e1);
+				}
+			}
+		}
+		Patient selectedPatient = null;
+		selectedPatient = result.getPatients().get(0);
+		Timestamp patient1LastUpdated = new Timestamp(Calendar.getInstance().getTime().getTime());
+		selectedPatient.setLastUpdated(patient1LastUpdated);
+		Study study1 = new Study("06/12/2010", "101010", "Test Study", "101.101");
+		Study study2 = new Study("06/12/2010", "202020", "Test Study", "202.202");
+		selectedPatient.addStudy(study1);
+		selectedPatient.addStudy(study2);
+		resultTree.updateNodes(result);
+		resultTree.setDoubleClicked(true);
+		resultTree.updateNodeProgressive(patientNode1);
+		synchronized(this){
+			while(selectedDataSearchResult.getPatients().get(0).getStudies().size() == 0){
+				try {
+					this.wait();
+				} catch (InterruptedException e1) {
+					logger.error(e1, e1);
+				}
+			}
+		}
+		selectedPatient = result.getPatients().get(0);
+		Study selectedStudy = selectedPatient.getStudy("202.202");
+		Timestamp study2LastUpdated = new Timestamp(Calendar.getInstance().getTime().getTime());
+		selectedStudy.setLastUpdated(study2LastUpdated);
+		Series series2 = new Series("2", "MIR", "Series Test", "202.202.1");
+		Series series3 = new Series("3", "MR", "Series Test", "303.303.1");
+		selectedStudy.addSeries(series2);
+		selectedStudy.addSeries(series3);
+		resultTree.updateNodes(result);
+		resultTree.setDoubleClicked(true);
+		StudyNode study2Node = (StudyNode)patientNode1.getChildAt(1);
+		resultTree.updateNodeProgressive(study2Node);
+		synchronized(this){
+			while(selectedDataSearchResult.getPatients().get(0).getStudies().get(1).getSeries().size() == 0){
+				try {
+					this.wait();
+				} catch (InterruptedException e1) {
+					logger.error(e1, e1);
+				}
+			}
+		}
+		List<Patient> patients = selectedDataSearchResult.getPatients();
+		int numbOfPatients = patients.size();
+		assertEquals("Invalid number of patients. Expected: 1, actual: " + numbOfPatients, 1, numbOfPatients);
+		if(numbOfPatients == 1) {
+			Patient patient = patients.get(0);
+			assertEquals("Invalid patient. Expected id: 111, actual: " + patient.getPatientID(), "111", patient.getPatientID());
+		}
+		List<Study> studies = patients.get(0).getStudies();
+		int numbOfStudies = studies.size();
+		assertEquals("Invalid number of studies. Expected: 2, actual: " + numbOfStudies, 2, numbOfStudies);
+		for(Study study : studies){
+			if(study.getStudyInstanceUID().equalsIgnoreCase("202.202")){
+				int numbOfSeriesInStudy2 = study.getSeries().size();
+				assertEquals("Invalid number of series in Study2. Expected: 2, actual: " + numbOfSeriesInStudy2, 2, numbOfSeriesInStudy2);
+			}
+		}
+	}
+	
+	//SearchResultTree 4A - basic flow. Two Patients found. Non selected. Then SelectAll executed.
+	//Result: selectedDataSearchResult should contain two patient.
+	@Test
+	public void testUpdatedSelectedDataSearchResult4A() {
+		resultTree.addDataSelectionListener(this);
+		Patient patient1 = new Patient("Jarek1", "111", "07/18/1973");
+		result.addPatient(patient1);
+		Patient patient2 = new Patient("Jarek2", "222", "07/18/1973");
+		result.addPatient(patient2);
+		resultTree.updateNodes(result);
+		resultTree.selectAll(true);
+		synchronized(this){
+			while(selectedDataSearchResult.getPatients().size() == 0){
+				try {
+					this.wait();
+				} catch (InterruptedException e1) {
+					logger.error(e1, e1);
+				}
+			}
+		}
+		//List<Patient> patients = selectedDataSearchResult.getPatients();
+		//int numbOfPatients = patients.size();
+		//assertEquals("Invalid number of patients. Expected: 2, actual: " + numbOfPatients, 2, numbOfPatients);
+		assertSelectAll(selectedDataSearchResult);
+	}
+
 
 	@Override
 	public void dataSelectionChanged(DataSelectionEvent event) {
@@ -183,65 +296,9 @@ public class UpdateSelectedDataSearchResultProgressiveTest implements DataSelect
 		for(Patient patient : patients){
 			List<Study> studies = patient.getStudies();
 			if(patient.getPatientID().equalsIgnoreCase("111")){
-				assertEquals("Invalid number of studies. Expected: 2, actual: " + studies.size(), 2, studies.size());
-				for(Study study : studies){
-					if(study.getStudyInstanceUID().equalsIgnoreCase("101.101")){
-						List<Series> series = study.getSeries();
-						assertEquals("Invalid number of series. Expected: 1, actual: " + series.size(), 1, series.size());
-						for(Series oneSeries : series){
-							if(oneSeries.getSeriesInstanceUID().equalsIgnoreCase("101.101.1")){
-								List<Item> items = oneSeries.getItems();
-								int numbOfItems = items.size();
-								assertEquals("Invalid number of items in Series 101.101.1 . Expected: 1, actual: " + numbOfItems, 1, numbOfItems);
-								assertEquals("Invalid item in Series 101.101.1 . Expected: 101.101.1.000.000.1, actual: " + items.get(0).getItemID(), "101.101.1.000.000.1", items.get(0).getItemID());
-							}
-						}
-					} else if (study.getStudyInstanceUID().equalsIgnoreCase("202.202")) {
-						List<Series> series = study.getSeries();
-						assertEquals("Invalid number of series. Expected: 2, actual: " + series.size(), 2, series.size());
-						for(Series oneSeries : series){
-							if(oneSeries.getSeriesInstanceUID().equalsIgnoreCase("202.202.1")){
-								List<Item> items = oneSeries.getItems();
-								int numbOfItems = items.size();
-								assertEquals("Invalid number of items in Series 202.202.1 . Expected: 2, actual: " + numbOfItems, 2, numbOfItems);
-								for(Item item : items){
-									if(item.getItemID().equalsIgnoreCase("202.202.1.000.000.1")){
-										assertEquals("Invalid item in Series 202.202.1 . Expected: 202.202.1.000.000.1, actual: " + item.getItemID(), "202.202.1.000.000.1", item.getItemID());
-									} else if (item.getItemID().equalsIgnoreCase("202.202.1.000.000.2")) {
-										assertEquals("Invalid item in Series 202.202.1 . Expected: 202.202.1.000.000.2, actual: " + item.getItemID(), "202.202.1.000.000.2", item.getItemID());
-									}
-								}
-							} else if(oneSeries.getSeriesInstanceUID().equalsIgnoreCase("303.303.1")){
-								List<Item> items = oneSeries.getItems();
-								int numbOfItems = items.size();
-								assertEquals("Invalid number of items in Series 303.303.1 . Expected: 2, actual: " + numbOfItems, 2, numbOfItems);
-								for(Item item : items){
-									if(item.getItemID().equalsIgnoreCase("303.303.1.000.000.1")){
-										assertEquals("Invalid item in Series 303.303.1 . Expected: 303.303.1.000.000.1, actual: " + item.getItemID(), "303.303.1.000.000.1", item.getItemID());
-									} else if (item.getItemID().equalsIgnoreCase("303.303.1.000.000.2")) {
-										assertEquals("Invalid item in Series 303.303.1 . Expected: 303.303.1.000.000.2, actual: " + item.getItemID(), "303.303.1.000.000.2", item.getItemID());
-									}
-								}
-							}
-						}
-					}
-				}
+				assertEquals("Invalid number of studies. Expected: 0, actual: " + studies.size(), 0, studies.size());
 			} else if (patient.getPatientID().equalsIgnoreCase("222")){
-				assertEquals("Invalid number of studies. Expected: 1, actual: " + studies.size(), 1, studies.size());
-				for(Study study : studies){
-					if(study.getStudyInstanceUID().equalsIgnoreCase("303.303")){
-						List<Series> series = study.getSeries();
-						assertEquals("Invalid number of series. Expected: 1, actual: " + series.size(), 1, series.size());
-						for(Series oneSeries : series){
-							if(oneSeries.getSeriesInstanceUID().equalsIgnoreCase("404.404.1")){
-								List<Item> items = oneSeries.getItems();
-								int numbOfItems = items.size();
-								assertEquals("Invalid number of items in Series 404.404.1 . Expected: 1, actual: " + numbOfItems, 1, numbOfItems);
-								assertEquals("Invalid item in Series 404.404.1 . Expected: 404.404.1.000.000.1, actual: " + items.get(0).getItemID(), "404.404.1.000.000.1", items.get(0).getItemID());
-							}
-						}
-					}
-				}
+				assertEquals("Invalid number of studies. Expected: 0, actual: " + studies.size(), 0, studies.size());
 			}
 		}
 	}
