c4b7803cf46d24231d636a69097d8358ecbb598d
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-12-12 20:12:06 +0000

    Renamed InsertDICOM class to PreloadDICOM. Modified main() method to runDicomShutDownSequence.
    SVN-Revision: 1053

--- src/edu/wustl/xipHost/dicom/PreloadDICOM.java
+++ src/edu/wustl/xipHost/dicom/PreloadDICOM.java
@@ -11,11 +11,14 @@ import edu.wustl.xipHost.localFileSystem.HostFileChooser;
  * @author Jaroslaw Krych
  *
  */
-public class InsertDICOM {
+public class PreloadDICOM {
 
 	public static void main(String[] args) {
 		HostFileChooser fileChooser = new HostFileChooser(true, new File("./dicom-dataset-demo"));
-		fileChooser.showOpenDialog(new JFrame());	
+		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fileChooser.showOpenDialog(frame);	
 		File[] files = fileChooser.getSelectedFiles();
 		if(files == null){
 			return;
@@ -24,7 +27,8 @@ public class InsertDICOM {
 		//PacsLocation loc = new PacsLocation("127.0.0.1", 3002, "WORKSTATION2", "XIPHost test database");
 		DicomManager dicomMgr = DicomManagerFactory.getInstance();
 		dicomMgr.runDicomStartupSequence();
-		DicomManagerFactory.getInstance().submit(files, loc);		
+		dicomMgr.submit(files, loc);
+		dicomMgr.runDicomShutDownSequence();
 		System.exit(0);
 	}
 }
