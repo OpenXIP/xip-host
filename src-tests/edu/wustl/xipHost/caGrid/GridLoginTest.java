bd02cd48132dded21af2234ab135074bcaa1ee7a
Krych, Jaroslaw <jarek.krych@gmail.com>
2011-12-20 14:23:00 +0000

    Added new GridLoginTest JUnit test.
    SVN-Revision: 1085

---
+++ src-tests/edu/wustl/xipHost/caGrid/GridLoginTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2011 Washington University in St. Louis. All Rights Reserved.
+ */
+package edu.wustl.xipHost.caGrid;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Jaroslaw Krych
+ *
+ */
+public class GridLoginTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
