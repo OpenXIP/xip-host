023581f5ba9fca854baf2a274df45320e5aab56c
Krych, Jaroslaw <jarek.krych@gmail.com>
2012-01-03 11:39:22 +0000

    Added new TestCheckboxTreePkgeAll JUnit suite.
    SVN-Revision: 1103

---
+++ src-tests/edu/wustl/xipHost/gui/checkboxTree/TestCheckboxTreePkgeAll.java
@@ -0,0 +1,12 @@
+package edu.wustl.xipHost.gui.checkboxTree;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UpdateSelectedDataSearchResultProgressiveTest.class,
+		UpdateSelectedDataSearchResultTest.class })
+public class TestCheckboxTreePkgeAll {
+
+}
