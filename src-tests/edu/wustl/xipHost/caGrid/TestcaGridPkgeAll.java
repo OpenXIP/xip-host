eba317f80033527e1445abc230d0c18a098b27e8
Krych, Jaroslaw <jarek.krych@gmail.com>
2012-02-14 07:33:03 +0000

    Modified TestcaGridPkgeAll JUit 3.8 suite.
    SVN-Revision: 1187

--- src-tests/edu/wustl/xipHost/caGrid/TestcaGridPkgeAll.java
+++ src-tests/edu/wustl/xipHost/caGrid/TestcaGridPkgeAll.java
@@ -12,19 +12,21 @@ import junit.framework.TestSuite;
  */
 public class TestcaGridPkgeAll {
 	public static Test suite(){
-		TestSuite suite = new TestSuite("Running all tests from caGrid package.");
+		TestSuite suite = new TestSuite("Running JUnit 3.8 tests from caGrid package.");
 		suite.addTestSuite(AddGridLocationTest.class);
-		suite.addTestSuite(ConvertToCQLTest.class);
 		suite.addTestSuite(GridLocationTest.class);
 		suite.addTestSuite(LoadGridLocationsTest.class);
-		suite.addTestSuite(LoadNCIAModelMapTest.class);
-		suite.addTestSuite(MapDicomTagToNCIATagNameTest.class);
 		suite.addTestSuite(ModifyGridLocationTest.class);
-		suite.addTestSuite(QueryGridLocationTest.class);
 		suite.addTestSuite(RemoveGridLocationTest.class);		
-		suite.addTestSuite(RetrieveDicomDataTest.class);
+		suite.addTestSuite(StoreGridLocationsTest.class);
 		suite.addTestSuite(RunGridStartupSequenceTest.class);
-		suite.addTestSuite(StoreGridLocationsTest.class);		
+		
+		
+		//suite.addTestSuite(ConvertToCQLTest.class);
+		//suite.addTestSuite(LoadNCIAModelMapTest.class);
+		//suite.addTestSuite(MapDicomTagToNCIATagNameTest.class);
+		//suite.addTestSuite(QueryGridLocationTest.class);
+		//suite.addTestSuite(RetrieveDicomDataTest.class);
 		return suite;
 		
 	}
