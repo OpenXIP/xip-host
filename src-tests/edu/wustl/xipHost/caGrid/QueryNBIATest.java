aa54a928e4ee15a92a776b42754eb0e533b9ad3b
Krych, Jaroslaw <jarek.krych@gmail.com>
2012-02-17 21:00:41 +0000

    Updated QueryNBIATest JUnit test. This JUnit test searches NBIA data source using NBIA specified implementation for accessing data (direct client call).
    SVN-Revision: 1191

--- src-tests/edu/wustl/xipHost/caGrid/QueryNBIATest.java
+++ src-tests/edu/wustl/xipHost/caGrid/QueryNBIATest.java
@@ -5,7 +5,6 @@ package edu.wustl.xipHost.caGrid;
 
 import static org.junit.Assert.*;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
-import gov.nih.nci.cagrid.cqlquery.QueryModifier;
 import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
 import gov.nih.nci.cagrid.data.MalformedQueryException;
 import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
@@ -13,19 +12,17 @@ import gov.nih.nci.cagrid.ncia.client.NCIACoreServiceClient;
 import gov.nih.nci.ivi.dicom.HashmapToCQLQuery;
 import gov.nih.nci.ivi.dicom.modelmap.ModelMap;
 import gov.nih.nci.ivi.dicom.modelmap.ModelMapException;
-import gov.nih.nci.ncia.domain.Image;
 import gov.nih.nci.ncia.domain.Series;
-import gov.nih.nci.ncia.domain.Study;
-
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import org.apache.axis.types.URI.MalformedURIException;
-import org.globus.wsrf.encoding.ObjectSerializer;
+import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -35,17 +32,12 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.namespace.QName;
 /**
  * @author Jaroslaw Krych
  *
  */
 public class QueryNBIATest {
+	final static Logger logger = Logger.getLogger(QueryNBIATest.class);
 	CQLQuery cqlQuery;
 	
 	/**
@@ -70,16 +62,15 @@ public class QueryNBIATest {
 	public void setUp() throws Exception {
 		HashMap<String, String> queryHashMap = new HashMap<String, String>();																		
 		if (queryHashMap.isEmpty()) {			
-			//queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Series.class.getCanonicalName());
+			queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Series.class.getCanonicalName());
 			//queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Study.class.getCanonicalName());
-			queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Image.class.getCanonicalName());
-			//queryHashMap.put("gov.nih.nci.ncia.domain.Study.studyInstanceUID", "1.3.6.1.4.1.9328.50.1.4717");
-			//queryHashMap.put("gov.nih.nci.ncia.domain.Series.instanceUID", "1.3.6.1.4.1.9328.50.1.4718");
-			queryHashMap.put("gov.nih.nci.ncia.domain.Patient.patientId", "1.3.6.1.4.1.9328.50.1.0008");
-			queryHashMap.put("gov.nih.nci.ncia.domain.Study.studyInstanceUID", "1.3.6.1.4.1.9328.50.1.4434");
-			queryHashMap.put("gov.nih.nci.ncia.domain.Series.instanceUID", "1.3.6.1.4.1.9328.50.1.4435");
+			//queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Image.class.getCanonicalName());
+			//queryHashMap.put(HashmapToCQLQuery.TARGET_NAME_KEY, Patient.class.getCanonicalName());
+			queryHashMap.put("gov.nih.nci.ncia.domain.Patient.patientId", "1.3.6.1.4.1.9328.50.1.0009");
+			queryHashMap.put("gov.nih.nci.ncia.domain.Study.studyInstanceUID", "1.3.6.1.4.1.9328.50.1.4717");
+			//queryHashMap.put("gov.nih.nci.ncia.domain.Series.instanceUID", "1.3.6.1.4.1.9328.50.1.4435");
 			//queryHashMap.put("gov.nih.nci.ncia.domain.Image.sopInstanceUID", "1.3.6.1.4.1.9328.50.1.4433");  
-		}else{
+		} else {
  
 		}
 		HashmapToCQLQuery h2cql;
@@ -88,13 +79,13 @@ public class QueryNBIATest {
 			
 			try {
 				cqlQuery = h2cql.makeCQLQuery(queryHashMap);
-				QueryModifier queryModifier = new QueryModifier();
+				/*QueryModifier queryModifier = new QueryModifier();
 				//String[] attributeNames = new String[1];
 				//attributeNames[0] = "gov.nih.nci.ncia.domain.Image.sopInstanceUID";
 				//queryModifier.setAttributeNames(attributeNames);
 				queryModifier.setCountOnly(true);
 				cqlQuery.setQueryModifier(queryModifier);
-				System.err.println(ObjectSerializer.toString(cqlQuery, new QName("http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery", "CQLQuery")));
+				System.err.println(ObjectSerializer.toString(cqlQuery, new QName("http://CQL.caBIG/1/gov.nih.nci.cagrid.CQLQuery", "CQLQuery")));*/
 			} catch (MalformedQueryException e) {
 				e.printStackTrace();
 			}
@@ -117,10 +108,13 @@ public class QueryNBIATest {
 	public void tearDown() throws Exception {
 	}
 
+	//INFO: Query is run against NBIA "http://imaging.nci.nih.gov/wsrf/services/cagrid/NCIACoreService" production service
+	//Basic flow (Series level query). Test run with NBIA implementation (as oppose to with GridQuery class). 
 	SAXBuilder builder = new SAXBuilder();
 	@Test
-	public void test() throws JDOMException, IOException {
+	public void testQueryNBIA_1A() throws JDOMException, IOException {
 		String gridServiceUrl= "http://imaging.nci.nih.gov/wsrf/services/cagrid/NCIACoreService";
+		List<String> expectedSeriesInstanceUID = new ArrayList<String>(); 
 		try {
 			NCIACoreServiceClient nciaClient = new NCIACoreServiceClient(gridServiceUrl);
 			CQLQueryResults result = nciaClient.query(cqlQuery);
@@ -128,30 +122,32 @@ public class QueryNBIATest {
 			    CQLQueryResultsIterator iter = new CQLQueryResultsIterator(result, true);
 			    while (iter.hasNext()) {
 				    String xmlSeries = (String)iter.next();
-				    System.out.println(xmlSeries);
+				    logger.debug(xmlSeries);
 				    Document document;
 				    Element root;
 				    InputStream is = new ByteArrayInputStream(xmlSeries.getBytes());
 				    document = builder.build(is);
 					root = document.getRootElement();
-					String objName = root.getName();
-					String nameSpace = root.getNamespacePrefix();
-					System.out.println(nameSpace);
-					//path for the parent of TMP directory
 					String seriesInstanceUID = root.getAttribute("instanceUID").getValue().trim();
+					expectedSeriesInstanceUID.add(seriesInstanceUID);
 					String modality = root.getAttribute("modality").getValue().trim();
-					System.out.println("Result series instance uid is " + seriesInstanceUID + " modality: " + modality );
+					logger.debug("Result series instance uid is " + seriesInstanceUID + " modality: " + modality );
 			     }
 			}else{
-			    System.out.println("No result found for ");
+			    logger.warn("No result found");
 			}
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e, e);
 		} catch (MalformedURIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e, e);
 		}
-		fail("Not yet implemented");
+		assertEquals("Number of found Series is different than expected.", expectedSeriesInstanceUID.size(), 3);
+		boolean resultsAsExpected = false;
+		if(expectedSeriesInstanceUID.contains("1.3.6.1.4.1.9328.50.1.4767") 
+				&& expectedSeriesInstanceUID.contains("1.3.6.1.4.1.9328.50.1.4718") 
+				&& expectedSeriesInstanceUID.contains("1.3.6.1.4.1.9328.50.1.4722")){
+			resultsAsExpected = true;
+		} 
+		assertTrue("Returned results are not as expected. ", resultsAsExpected);
 	}
 }
