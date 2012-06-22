d09d303cd457e70b3520da26bfb7f27aa18d5bd4
Krych, Jaroslaw <jarek.krych@gmail.com>
2012-06-22 21:38:28 +0000

    Modified XIPHostCompileRun.sh and XIPHostRun.sh files.
    SVN-Revision: 1342

--- XIPHostCompileRun.sh
+++ XIPHostCompileRun.sh
@@ -1,1 +1,5 @@
-ant clean all run
+#! /usr/bin/env bash
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+ant clean all run -buildfile $BASEDIR/build.xml
+read -p "Press any key to continue..."
--- XIPHostRun.sh
+++ XIPHostRun.sh
@@ -1,1 +1,5 @@
-ant run
+#! /usr/bin/env bash
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+ant run -buildfile $BASEDIR/build.xml
+read -p "Press any key to continue..."
